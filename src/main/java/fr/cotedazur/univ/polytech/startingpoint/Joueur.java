a1484492c89a27caa73558e4966ace1e0e2024ef
ChloeVandenbrulle <chloe.vandenbrulle@etu.unice.fr>
2022-12-07 18:07:35 +0100

    task:création classe Joueur
    #3

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Joueur.java
@@ -0,0 +1,16 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class Joueur {
+    private String nom;
+
+    public Joueur(String nom){
+        this.nom = nom;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+}
