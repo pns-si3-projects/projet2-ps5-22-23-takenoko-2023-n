2f972ea95dac46ce446b56293c7127b4f54de7fe
amandinemart1 <amandine.martin@etu.unic-cotedazur.fr>
2022-12-08 09:33:03 +0100

    task: ajout de la position a l'etang
    #10

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Etang.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Etang.java
@@ -2,5 +2,14 @@ package fr.cotedazur.univ.polytech.startingpoint;
 
 public class Etang {
 
+    private final Position position=new Position();
     public Etang(){}
+
+    /**
+     * getter de l'attribut position
+     * @return la valeur de la position
+     */
+    public Position getPosition() {
+        return position;
+    }
 }
