bcc4761c68d79e463e962565abc074a6ce6ecab4
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-08 23:55:38 +0100

    task: Modification main pour s'adapter au Jeu avec Plateau
    #21

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -3,9 +3,8 @@ package fr.cotedazur.univ.polytech.startingpoint;
 public class Main {
     public static void main(String[] args){
         Joueur joueur = new Joueur("Robot1");
-        Etang etang = new Etang();
-        Jeu jeu = new Jeu(joueur, etang);
-        System.out.println(jeu.toString());
+        Jeu jeu = new Jeu(joueur);
+        System.out.println(jeu);
     }
 
 }
