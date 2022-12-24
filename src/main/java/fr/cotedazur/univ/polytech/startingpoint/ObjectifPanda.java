546429008b99d290a0604db4e1e5df14a8af4389
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-24 11:30:42 +0100

    task : Création de ObjectifPanda
    #88

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ObjectifPanda.java
@@ -0,0 +1,36 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class ObjectifPanda extends Objectif {
+    // Définition des attributs
+    private int nombreBambousAManger;
+
+
+    // Définition des constructeurs
+    /**
+     * Constructeur par défaut
+     * @param nbPoints > 0 est le nombre de points de l'objectif
+     * @param nbBambous > 0 est le nombre de bambous que le joueur doit posséder
+     */
+    public ObjectifPanda(int nbPoints, int nbBambous) {
+        nombrePoints = nbPoints;
+        nombreBambousAManger = nbBambous;
+    }
+
+
+    // Accesseurs et méthode toString
+    /**
+     * Renvoie le nombre de bambous que le joueur doit posséder
+     * @return le nombre de bambous que le joueur doit posséder
+     */
+    public int getNombreBambousAManger() {
+        return nombreBambousAManger;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Objectif de ";
+        s += nombreBambousAManger + " bambous et vaut ";
+        s += nombrePoints + " points";
+        return s;
+    }
+}
