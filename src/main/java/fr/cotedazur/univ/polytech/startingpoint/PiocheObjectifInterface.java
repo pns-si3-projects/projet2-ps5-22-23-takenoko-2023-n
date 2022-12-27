495c368de613c90a7765dda9bd9c258434146e51
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-27 17:05:00 +0100

    task: Création de l'interface pour une pioche d'objectifs
    #88

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/PiocheObjectifInterface.java
@@ -0,0 +1,16 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public interface PiocheObjectifInterface {
+    /**
+     * Renvoie le nombre de cartes que contient la pioche
+     * @return le nombre de cartes dans la pioche
+     */
+    public int getNombreObjectifs();
+
+    /**
+     * Renvoie une carte objectif désignée dans la pioche
+     * @return la carte objectif piochée
+     * @implNote La pioche ne doit pas être vide
+     */
+    public Objectif pioche();
+}
