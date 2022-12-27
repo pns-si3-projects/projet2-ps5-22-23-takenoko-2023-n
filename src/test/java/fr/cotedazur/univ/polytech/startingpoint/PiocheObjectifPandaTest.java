82d6e52574281b8884e36bbb2e60f6a4c143fc77
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-27 16:26:30 +0100

    test: Création PiocheObjectifPandaTest
    #92

---
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/PiocheObjectifPandaTest.java
@@ -0,0 +1,48 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class PiocheObjectifPandaTest {
+    PiocheObjectifPanda piocheObjectifPanda;
+    @Mock Random mockRandom = mock(Random.class);
+
+    @Test
+    void getNombreObjectifs() {
+        piocheObjectifPanda = new PiocheObjectifPanda(new Random());
+        assertEquals(15, piocheObjectifPanda.getNombreObjectifs());
+    }
+
+    @Test
+    void piocheValeurTropGrande() {
+        when(mockRandom.nextInt(anyInt())).thenReturn(15);
+        piocheObjectifPanda = new PiocheObjectifPanda(mockRandom);
+        assertThrows(RuntimeException.class, () -> {piocheObjectifPanda.pioche();});
+    }
+
+    @Test
+    void piocheValeurTropPetite() {
+        when(mockRandom.nextInt(anyInt())).thenReturn(-1);
+        piocheObjectifPanda = new PiocheObjectifPanda(mockRandom);
+        assertThrows(RuntimeException.class, () -> {piocheObjectifPanda.pioche();});
+    }
+
+    @Test
+    void pioche() {
+        when(mockRandom.nextInt(anyInt())).thenReturn(14, 1, 4, 9, 9);
+        piocheObjectifPanda = new PiocheObjectifPanda(mockRandom);
+        assertEquals(new ObjectifPanda(6, 3), piocheObjectifPanda.pioche());
+        assertEquals(new ObjectifPanda(3, 2), piocheObjectifPanda.pioche());
+        // car en supprimant 1, les suivant sont décalés donc on prend ancien 5
+        assertEquals(new ObjectifPanda(4, 2), piocheObjectifPanda.pioche());
+        assertEquals(new ObjectifPanda(5, 2), piocheObjectifPanda.pioche());
+        assertEquals(new ObjectifPanda(6, 3), piocheObjectifPanda.pioche());
+    }
+}
