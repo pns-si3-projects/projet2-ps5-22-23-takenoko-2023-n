c45a8979cfde926302fde40ab51d98c261b473c8
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-30 19:00:58 +0100

    task: Création PiocheBambou
    Ajouter un object random et la gestion de l'aléatoire quand il y aura les couleurs des bambous #90

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/PiocheBambou.java
@@ -0,0 +1,53 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+/**
+ * Gère les pioches de bambous
+ * @author équipe N
+ */
+public class PiocheBambou {
+    // Définition des attributs
+    private int nombreBambous;
+
+
+    /**
+     * Constructeur par défaut, crée une pioche de 90 sections de bambous
+     */
+    public PiocheBambou() {
+        nombreBambous = 90;
+    }
+
+
+    // Accesseurs et méthode toString
+    /**
+     * Renvoie le nombre de sections de bambous que contient la pioche
+     * @return le nombre de sections de bambous restantes dans la pioche
+     */
+    public int getNombreBambousRestants() {
+        return nombreBambous;
+    }
+
+    /**
+     * Renvoie si la pioche de bambous est vide
+     * @return true la pioche est vide, false sinon
+     */
+    public boolean isEmpty() {
+        return nombreBambous == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Pioche de bambous : " + getNombreBambousRestants() + " sections.";
+    }
+
+
+    // Méthodes d'utilisation
+    /**
+     * Renvoie une section de bambou désignée dans la pioche
+     * @return la section de bambou piochée
+     * @implNote la pioche ne doit pas être vide
+     */
+    public SectionBambou pioche() {
+        assert !isEmpty() : "La pioche de bambous est vide";
+        return new SectionBambou();
+    }
+}
