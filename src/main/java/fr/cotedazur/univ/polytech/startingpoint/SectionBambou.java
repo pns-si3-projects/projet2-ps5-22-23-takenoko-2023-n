db52808489d4a3f7b37f8e16e112eb613319feb9
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-30 18:41:41 +0100

    task: Création SectionBambou
    A completer plus tard #90

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/SectionBambou.java
@@ -0,0 +1,21 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+/**
+ * Classe représentant une section de SectionBambou
+ * @author equipe N
+ */
+public class SectionBambou {
+    // Définition des attributs
+
+
+
+    // Définition des constructeurs
+    /**
+     * Constructeur par défaut
+     */
+    public SectionBambou() {
+    }
+
+
+    // Accesseurs et méthode toString et equals
+}
