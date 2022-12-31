5236a12f2ac48a245a69faba482eb7b61a7c228d
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-31 11:03:34 +0100

    test: adaptation PiocheBambouTest au random
    #92

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/PiocheBambouTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/PiocheBambouTest.java
@@ -2,32 +2,62 @@ package fr.cotedazur.univ.polytech.startingpoint;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PiocheBambouTest {
     PiocheBambou piocheBambou;
-
-    @BeforeEach
-    void setUp() {
-        piocheBambou = new PiocheBambou();
-    }
+    @Mock
+    Random mockRandom = mock(Random.class);
 
     @Test
     void getNombreBambousRestants() {
+        piocheBambou = new PiocheBambou(new Random());
         assertEquals(90, piocheBambou.getNombreBambousRestants());
         piocheBambou.pioche();
+        assertEquals(89, piocheBambou.getNombreBambousRestants());
         piocheBambou.pioche();
-        assertEquals(88, piocheBambou.getNombreBambousRestants());
+        piocheBambou.pioche();
+        assertEquals(87, piocheBambou.getNombreBambousRestants());
     }
 
     @Test
     void isEmpty() {
+        piocheBambou = new PiocheBambou(new Random());
+        assertFalse(piocheBambou.isEmpty());
+        piocheBambou.pioche();
         assertFalse(piocheBambou.isEmpty());
     }
 
     @Test
+    void piocheValeurTropGrande() {
+        when(mockRandom.nextInt(anyInt())).thenReturn(90);
+        piocheBambou = new PiocheBambou(mockRandom);
+        assertThrows(RuntimeException.class, () -> {piocheBambou.pioche();});
+    }
+
+    @Test
+    void piocheValeurTropPetite() {
+        when(mockRandom.nextInt(anyInt())).thenReturn(-1);
+        piocheBambou = new PiocheBambou(mockRandom);
+        assertThrows(RuntimeException.class, () -> {piocheBambou.pioche();});
+    }
+
+    @Test
     void pioche() {
+        // retirer le ".getClass()" lorsque le equals de la classe SectionBambou sera faisable et fait (avec la couleur)
+        when(mockRandom.nextInt(anyInt())).thenReturn(89, 1, 4, 28, 28);
+        piocheBambou = new PiocheBambou(mockRandom);
+        assertEquals(new SectionBambou().getClass(), piocheBambou.pioche().getClass());
+        assertEquals(new SectionBambou().getClass(), piocheBambou.pioche().getClass());
+        // car en supprimant 1, les suivant sont décalés donc on prend ancien 5
+        assertEquals(new SectionBambou().getClass(), piocheBambou.pioche().getClass());
         assertEquals(new SectionBambou().getClass(), piocheBambou.pioche().getClass());
         assertEquals(new SectionBambou().getClass(), piocheBambou.pioche().getClass());
     }
