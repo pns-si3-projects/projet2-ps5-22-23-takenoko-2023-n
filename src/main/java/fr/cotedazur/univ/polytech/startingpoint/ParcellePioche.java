68e35d25e892eb48f8bd17cdf191a396e028fde1
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2022-12-31 11:10:22 +0100

    task: Création ParcellePioche
    Représente une ParcelleCouleur mais sans position #84

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ParcellePioche.java
@@ -0,0 +1,22 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+/**
+ * Répresente une parcelle dans la pioche -> parcelle sans position
+ */
+public class ParcellePioche {
+    // Définition des attributs
+
+
+
+    // Définition des constructeurs
+    /**
+     * Constructeur par défaut
+     */
+    public ParcellePioche() {
+
+    }
+
+
+    // Accesseurs et méthode toString et equals
+
+}
