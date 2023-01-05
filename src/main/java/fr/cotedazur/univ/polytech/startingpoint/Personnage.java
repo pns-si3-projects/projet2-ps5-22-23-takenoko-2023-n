05dfdbe848a0a69a6957c807f5b15d39374dc23a
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-05 18:42:07 +0100

    task: renommage Positionnable en Positionable
    #100

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Personnage.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Personnage.java
@@ -4,5 +4,5 @@ package fr.cotedazur.univ.polytech.startingpoint;
  * Interface représentant un personnage sur le plateau (Panda ou Jardinier)
  * @author equipe N
  */
-public interface Personnage extends Positionnable {
+public interface Personnage extends Positionable {
 }
