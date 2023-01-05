d29de2f1cf7b81c62ad0fd36789bac12788f88e1
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-05 18:34:03 +0100

    test: Création PlaquetteTest
    #77

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/PlaquetteTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/PlaquetteTest.java
@@ -7,16 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlaquetteTest {
     Plaquette plaquette;
+    SectionBambou secBa;
+    ObjectifParcelle objPar2_3;
+    ObjectifParcelle objPar3_4;
+    ObjectifPanda objPan3_2;
+    ObjectifPanda objPan4_2;
+    ObjectifJardinier objJar3_2;
+    ObjectifJardinier objJar6_4;
 
     @BeforeEach
     void setUp() {
         plaquette = new Plaquette();
+        secBa = new SectionBambou();
+        objPar2_3 = new ObjectifParcelle(2, 3);
+        objPar3_4 = new ObjectifParcelle(3, 4);
+        objPan3_2 = new ObjectifPanda(3, 2);
+        objPan4_2 = new ObjectifPanda(4, 2);
+        objJar3_2 = new ObjectifJardinier(3, 2);
+        objJar6_4 = new ObjectifJardinier(6, 4);
         plaquette.ajouteSectionBambou(new SectionBambou()); // modifier pour vert
         plaquette.ajouteSectionBambou(new SectionBambou()); // modifier pour rose
         plaquette.ajouteSectionBambou(new SectionBambou()); // modifier pour jaune
-        plaquette.ajouteObjectif(new ObjectifParcelle(2, 3));
-        plaquette.ajouteObjectif(new ObjectifPanda(3, 2));
-        plaquette.ajouteObjectif(new ObjectifJardinier(5, 4));
+        try {
+            plaquette.ajouteObjectif(objPar2_3);
+            plaquette.ajouteObjectif(objPan3_2);
+            plaquette.ajouteObjectif(objJar3_2);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
     }
 
     @Test
@@ -46,33 +64,48 @@ class PlaquetteTest {
     @Test
     void getNombreObjectifParcelle() {
         assertEquals(1, plaquette.getNombreObjectifParcelle());
-        plaquette.ajouteObjectif(new ObjectifParcelle(3, 4));
-        plaquette.ajouteObjectif(new ObjectifPanda(4, 2));
+        try {
+            plaquette.ajouteObjectif(objPar3_4);
+            plaquette.ajouteObjectif(objPan4_2);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
         assertEquals(2, plaquette.getNombreObjectifParcelle());
     }
 
     @Test
     void getNombreObjectifPanda() {
         assertEquals(1, plaquette.getNombreObjectifPanda());
-        plaquette.ajouteObjectif(new ObjectifParcelle(3, 4));
-        plaquette.ajouteObjectif(new ObjectifPanda(4, 2));
+        try {
+            plaquette.ajouteObjectif(objPar3_4);
+            plaquette.ajouteObjectif(objPan4_2);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
         assertEquals(2, plaquette.getNombreObjectifPanda());
     }
 
     @Test
     void getNombreObjectifJardinier() {
         assertEquals(1, plaquette.getNombreObjectifJardinier());
-        plaquette.ajouteObjectif(new ObjectifParcelle(3, 4));
-        plaquette.ajouteObjectif(new ObjectifJardinier(6, 4));
+        try {
+            plaquette.ajouteObjectif(objPar3_4);
+            plaquette.ajouteObjectif(objJar6_4);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
         assertEquals(2, plaquette.getNombreObjectifJardinier());
     }
 
     @Test
     void getNombreObjectifs() {
         assertEquals(3, plaquette.getNombreObjectifs());
-        plaquette.ajouteObjectif(new ObjectifParcelle(3, 4));
-        assertEquals(4, plaquette.getNombreObjectifs());
-        plaquette.ajouteObjectif(new ObjectifPanda(4, 2));
+        try {
+            plaquette.ajouteObjectif(objPan4_2);
+            plaquette.ajouteObjectif(objJar6_4);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
         assertEquals(5, plaquette.getNombreObjectifs());
     }
 
@@ -84,25 +117,86 @@ class PlaquetteTest {
 
     @Test
     void getObjectifsParcelle() {
+        assertEquals(objPar2_3, plaquette.getObjectifsParcelle()[0]);
+        try {
+            plaquette.ajouteObjectif(objPar3_4);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
+        // Parce que pris dans l'ordre inverse d'ajout
+        assertNotEquals(objPar2_3, plaquette.getObjectifsParcelle()[0]);
+        assertEquals(objPar3_4, plaquette.getObjectifsParcelle()[0]);
+        assertEquals(objPar2_3, plaquette.getObjectifsParcelle()[1]);
     }
 
     @Test
     void getObjectifsPanda() {
+        assertEquals(objPan3_2, plaquette.getObjectifsPanda()[0]);
+        try {
+            plaquette.ajouteObjectif(objPan4_2);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
+        // Parce que pris dans l'ordre inverse d'ajout
+        assertNotEquals(objPan3_2, plaquette.getObjectifsPanda()[0]);
+        assertEquals(objPan4_2, plaquette.getObjectifsPanda()[0]);
+        assertEquals(objPan3_2, plaquette.getObjectifsPanda()[1]);
     }
 
     @Test
     void getObjectifsJardinier() {
+        assertEquals(objJar3_2, plaquette.getObjectifsJardinier()[0]);
+        try {
+            plaquette.ajouteObjectif(objJar6_4);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
+        // Parce que pris dans l'ordre inverse d'ajout
+        assertNotEquals(objJar3_2, plaquette.getObjectifsJardinier()[0]);
+        assertEquals(objJar6_4, plaquette.getObjectifsJardinier()[0]);
+        assertEquals(objJar3_2, plaquette.getObjectifsJardinier()[1]);
     }
 
     @Test
     void getObjectifs() {
+        // Parce que pris dans l'ordre inverse d'ajout
+        assertEquals(objPar2_3, plaquette.getObjectifs()[2]);
+        assertEquals(objPan3_2, plaquette.getObjectifs()[1]);
+        assertEquals(objJar3_2, plaquette.getObjectifs()[0]);
+        try {
+            plaquette.ajouteObjectif(objPan4_2);
+            plaquette.ajouteObjectif(objJar6_4);
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
+        assertEquals(objJar6_4, plaquette.getObjectifs()[0]);
+        assertEquals(objPan4_2, plaquette.getObjectifs()[1]);
+        assertEquals(objPar2_3, plaquette.getObjectifs()[4]);
     }
 
     @Test
     void ajouteSectionBambou() {
+        assertEquals(3, plaquette.getNombreBambousVerts());
+        plaquette.ajouteSectionBambou(new SectionBambou()); // Modifier pour ajout bambou vert
+        plaquette.ajouteSectionBambou(new SectionBambou()); // Modifier pour ajout bambou jaune
+        plaquette.ajouteSectionBambou(new SectionBambou()); // Modifier pour ajout bambou rose
+        plaquette.ajouteSectionBambou(new SectionBambou()); // Modifier pour ajout bambou vert
+        plaquette.ajouteSectionBambou(new SectionBambou()); // Modifier pour ajout bambou vert
+        plaquette.ajouteSectionBambou(new SectionBambou()); // Modifier pour ajout bambou jaune
+        assertEquals(9, plaquette.getNombreBambousVerts()); // Modifier pour 6 verts
+        assertEquals(9, plaquette.getNombreBambousRoses()); // Modifier pour 4 roses
+        assertEquals(9, plaquette.getNombreBambousJaunes()); // Modifier pour 5 jaunes
     }
 
     @Test
     void ajouteObjectif() {
+        try {
+            plaquette.ajouteObjectif(objPar3_4);
+            plaquette.ajouteObjectif(objPan4_2);
+            assertEquals(5, plaquette.getNombreObjectifs());
+        } catch (NombreObjectifsEnCoursException nOECE) {
+            throw new AssertionError(nOECE);
+        }
+        assertThrows(NombreObjectifsEnCoursException.class, () -> {plaquette.ajouteObjectif(objJar6_4);});
     }
 }
