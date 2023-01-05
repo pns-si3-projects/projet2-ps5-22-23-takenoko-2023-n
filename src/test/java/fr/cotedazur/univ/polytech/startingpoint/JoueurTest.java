985dde168088d30c78849f8144df464b1011d69d
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-05 22:42:04 +0100

    test: petite correction
    #78

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/JoueurTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/JoueurTest.java
@@ -3,14 +3,19 @@ package fr.cotedazur.univ.polytech.startingpoint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JoueurTest {
     Joueur joueur1;
 
     @BeforeEach
-    void setUp(){
-        joueur1 = new Joueur("Robot1");
+    void setUp() {
+        ObjectifParcelle objPar2_3 = new ObjectifParcelle(2, 3);
+        ObjectifPanda objPan3_2 = new ObjectifPanda(3, 2);
+        ObjectifJardinier objJar3_2 = new ObjectifJardinier(3, 2);
+        joueur1 = new Joueur("Robot1", new Random(), objPar2_3, objPan3_2, objJar3_2);
     }
 
     @Test
