871ec403b78ab6e99616dcd5b47c5261d8a734e3
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-01-06 02:24:00 +0100

    test: Ajout de test d'arbitre
    Ajout des 4 tests d'arbitre #96

---
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/ArbitreTest.java
@@ -0,0 +1,81 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArbitreTest {
+    Joueur joueur1;
+    Joueur joueur2;
+    Arbitre arbitre;
+    PiocheObjectif piocheObjectif;
+    Plateau plateau;
+
+    @BeforeEach
+    void setUp(){
+        arbitre = new Arbitre();
+        piocheObjectif = new PiocheObjectif(new PiocheObjectifParcelle(new Random()),new PiocheObjectifPanda(new Random()),new PiocheObjectifJardinier(new Random()));
+        plateau = new Plateau();
+        joueur1 = new Joueur("Robot1",new Random(),piocheObjectif.piocheObjectifParcelle());
+        joueur2 = new Joueur("Robot2",new Random(),piocheObjectif.piocheObjectifParcelle());
+    }
+
+    @Test
+    void checkFinDeJeu() {
+        while (!arbitre.checkFinDeJeu(joueur1,joueur2)){
+            assertFalse(arbitre.checkFinDeJeu(joueur1,joueur2));
+            joueur1.tour(piocheObjectif,plateau,arbitre);
+            joueur2.tour(piocheObjectif,plateau,arbitre);
+        }
+        assertTrue(arbitre.checkFinDeJeu(joueur1,joueur2));
+    }
+
+    @Test
+    void joueurGagnant() {
+        assertTrue(arbitre.joueurGagnant(joueur1,joueur2).isEmpty());
+        while (!arbitre.checkFinDeJeu(joueur1,joueur2)){
+            joueur1.tour(piocheObjectif,plateau,arbitre);
+            joueur2.tour(piocheObjectif,plateau,arbitre);
+        }
+        if(joueur1.getPoint() > joueur2.getPoint()){
+            assertEquals(Optional.of(joueur1),arbitre.joueurGagnant(joueur1,joueur2));
+        }
+        else if(joueur1.getPoint() < joueur2.getPoint()){
+            assertEquals(Optional.of(joueur2),arbitre.joueurGagnant(joueur1,joueur2));
+        }
+        else{
+            assertTrue(arbitre.joueurGagnant().isEmpty());
+        }
+    }
+
+    @Test
+    void checkObjectifParcelleTermine() {
+        ObjectifParcelle objectifParcelleACheck = new ObjectifParcelle(5,4);
+        objectifParcelleACheck.setNombreParcellePresenteEnJeu(1);
+        int i = 0;
+        while(i < 4){
+            assertFalse(arbitre.checkObjectifParcelleTermine(plateau.getParcelles(), objectifParcelleACheck));
+            try {
+                ParcelleCouleur parcelleCouleurAAdd = new ParcelleCouleur(plateau.getPositionsDisponible()[0]);
+                plateau.addParcelle(parcelleCouleurAAdd);
+            }
+            catch (ParcelleExistanteException | NombreParcelleVoisineException exception){
+                assert false: "Ne doit pas renvoyer d'erreur normalement";
+            }
+            i++;
+        }
+        assertTrue(arbitre.checkObjectifParcelleTermine(plateau.getParcelles(), objectifParcelleACheck));
+    }
+
+    @Test
+    void getNombreTour() {
+        assertEquals(arbitre.getNombreTour(),1);
+        arbitre.addTour();
+        arbitre.addTour();
+        assertEquals(arbitre.getNombreTour(),3);
+    }
+}
