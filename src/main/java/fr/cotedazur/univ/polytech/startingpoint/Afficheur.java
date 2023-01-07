4cd239856fdd235d903de125560b8a8ad1f7a9e9
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-01-07 18:26:18 +0100

    documentation: ajout commentaire de classe Afficheur
    Ajout des commentaires et le code un peu plus 'propre' #94

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Afficheur.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Afficheur.java
@@ -2,56 +2,92 @@ package fr.cotedazur.univ.polytech.startingpoint;
 
 import java.util.Optional;
 
+/**
+ * Classe représentant l'affichage du jeu
+ * @author equipe N
+ */
 public class Afficheur {
 
+    /**
+     * Affiche le gagnant du jeu si il y en a un
+     * @param joueur le joueur gagnant
+     */
     public void afficheGagnant(Optional<Joueur> joueur){
-        if(joueur.isEmpty()){
+        if ( joueur.isEmpty() ) {
             System.out.println("Il y a eu une égalité");
         }
         else {
             Joueur joueurGagnant = joueur.get();
-            System.out.println("Le joueur gagnant est le joueur "+joueurGagnant.getNom()+" avec "+joueurGagnant.getPoints()+" Points");
+            System.out.println("Le joueur gagnant est le joueur " + joueurGagnant.getNom() + " avec " + joueurGagnant.getPoints() + " Points");
         }
     }
 
+    /**
+     * Affiche l'ajout d'une parcelle sur le plateau ainsi que sa position
+     * @param parcelleCouleurAjoute la parcelle couleur ajouté
+     */
     public void afficheAjoutParcelle(ParcelleCouleur parcelleCouleurAjoute){
-        if(parcelleCouleurAjoute != null){
-            System.out.println("La "+parcelleCouleurAjoute+" est pose");
+        if ( parcelleCouleurAjoute != null ) {
+            System.out.println("La " + parcelleCouleurAjoute + " est pose");
         }
     }
 
+    /**
+     * Affiche le début d'un nouveau tour
+     * @param nombreDeTour Le numéro du tour
+     */
     public void afficheDebutTour(int nombreDeTour){
-        System.out.println("Tour numero"+nombreDeTour);
+        System.out.println("Tour numero" + nombreDeTour);
     }
 
+    /**
+     * Affiche la fin d'un tour
+     * @param nombreDeTour Le numéro du tour
+     */
     public void afficheFinTour(int nombreDeTour){
-        System.out.println("Tour numéro"+nombreDeTour+" terminé");
+        System.out.println("Tour numéro" + nombreDeTour + " terminé");
         System.out.println("\n-------------------------------------------------\n");
     }
 
+    /**
+     * Affiche le début du tour du joueur
+     * @param joueur le joueur commençant son tour
+     */
     public void afficheJoueurDebutTour(Joueur joueur){
-        System.out.println("Au tour du joueur "+joueur.getNom());
+        System.out.println("Au tour du joueur " + joueur.getNom());
     }
 
+    /**
+     * Affiche la fin du tour du joueur
+     * @param joueur le joueur finissant son tour
+     */
     public void afficheJoueurFinTour(Joueur joueur){
-        System.out.println("Fin du tour du joueur "+joueur.getNom()+"\n");
+        System.out.println("Fin du tour du joueur " + joueur.getNom() + "\n");
     }
 
+    /**
+     * Affiche l'objectif pioché
+     * @param objectif l'objectif pioché
+     */
     public void affichePiocheCarte(Objectif objectif){
         String messageAAffiche;
-        if(objectif.getClass() == ObjectifParcelle.class){
+        if ( objectif.getClass() == ObjectifParcelle.class ) {
             messageAAffiche = "Parcelle";
         }
-        else if(objectif.getClass() == ObjectifPanda.class){
+        else if ( objectif.getClass() == ObjectifPanda.class ){
             messageAAffiche = "Panda";
         }
         else {
             messageAAffiche = "Jardinier";
         }
-        System.out.println("Objectif "+messageAAffiche+" pioché");
+        System.out.println("Objectif " + messageAAffiche + " pioché");
     }
 
+    /**
+     * Affiche l'objectif terminé
+     * @param objectif L'objectif qui vient de se terminé
+     */
     public void afficheObjectifValide(Objectif objectif){
-        System.out.println(objectif+" terminé");
+        System.out.println(objectif + " terminé");
     }
 }
