2e156744c49e225a87122b02fe6eb58beb309ffe
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-01-12 20:38:38 +0100

    test: ajout du test de Section Bambou
    Ajout des tests pour la classe SectionBambouTest (2Tests) #114

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/plateau/SectionBambouTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/plateau/SectionBambouTest.java
@@ -1,16 +1,41 @@
 package fr.cotedazur.univ.polytech.startingpoint.plateau;
 
-import fr.cotedazur.univ.polytech.startingpoint.plateau.SectionBambou;
+import fr.cotedazur.univ.polytech.startingpoint.Couleur;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SectionBambouTest {
-    SectionBambou bambou;
+    SectionBambou bambouVert;
+    SectionBambou bambouJaune;
+    SectionBambou bambouRose;
+    SectionBambou bambouVertBis;
 
     @BeforeEach
     void setUp() {
-        bambou = new SectionBambou();
+        bambouVert = new SectionBambou(Couleur.VERT);
+        bambouJaune = new SectionBambou(Couleur.JAUNE);
+        bambouRose = new SectionBambou(Couleur.ROSE);
+        bambouVertBis = new SectionBambou(Couleur.VERT);
+    }
+
+    @Test
+    void getCouleur() {
+        assertEquals(Couleur.VERT,bambouVert.getCouleur());
+        assertNotEquals(Couleur.JAUNE,bambouRose.getCouleur());
+        assertEquals(Couleur.ROSE,bambouRose.getCouleur());
+        assertEquals(Couleur.JAUNE,bambouJaune.getCouleur());
+        assertEquals(Couleur.VERT,bambouVertBis.getCouleur());
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(bambouVert,bambouVertBis);
+        assertNotEquals(bambouVert,bambouJaune);
+        assertNotEquals(bambouVert,bambouRose);
+        assertThrows(NullPointerException.class, () -> {new SectionBambou(null);});
     }
 }
