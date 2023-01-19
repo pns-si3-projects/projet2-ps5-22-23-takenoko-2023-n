cf77de21c5175f5f9d065acf49fee50868bf2e68
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-19 16:23:45 +0100

    test: Modification PlaquetteTest après plaquette vidée
    #149

---
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/joueur/PlaquetteTest.java
@@ -0,0 +1,38 @@
+package fr.cotedazur.univ.polytech.startingpoint.joueur;
+
+import fr.cotedazur.univ.polytech.startingpoint.plateau.SectionBambou;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlaquetteTest {
+    // Attributs
+    Plaquette plaquette;
+
+
+    // Constructeur
+    @BeforeEach
+    void setUp() {
+        plaquette = new Plaquette();
+    }
+
+
+    // Accesseurs
+    @Test
+    void getReserveBambousManges() {
+        assertEquals(new SectionBambou[0], plaquette.getReserveBambousManges());
+    }
+
+    @Test
+    void isActionTour() {
+    }
+
+    @Test
+    void nombreBambouCouleur() {
+    }
+
+    @Test
+    void mangeSectionBambou() {
+    }
+}
