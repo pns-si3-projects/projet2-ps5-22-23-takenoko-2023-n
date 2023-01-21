810250257187174bc28d6fdbf6205b064354a77a
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-21 16:49:58 +0100

    task: ajout commentaire afficheur
    #172

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Afficheur.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Afficheur.java
@@ -1,10 +1,13 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
 /**
- * Affiche les changements dans le jeu
+ * Affiche les changements dans le jeu.
  * @author equipe N
  */
 public class Afficheur {
+    /**
+     * Affiche l'état d'initialisation du jeu
+     */
     public static void initialisation() {
         System.out.println("Initialisation du jeu : étang, panda et jardinier en (0,0).");
     }
