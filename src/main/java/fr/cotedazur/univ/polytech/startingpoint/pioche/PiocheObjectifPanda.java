8f84afd2e303395117a65b5d752d32c296eec7ba
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-22 21:19:49 +0100

    task: Correction gestion objectifs PiocheObjectifPanda
    #171

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheObjectifPanda.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheObjectifPanda.java
@@ -4,8 +4,9 @@ import fr.cotedazur.univ.polytech.startingpoint.jeu.Couleur;
 import fr.cotedazur.univ.polytech.startingpoint.objectif.Objectif;
 import fr.cotedazur.univ.polytech.startingpoint.objectif.ObjectifPanda;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Range;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -15,19 +16,39 @@ import java.util.Random;
 public class PiocheObjectifPanda implements PiocheObjectifInterface {
     // Définition des attributs
 
+    private final List<ObjectifPanda> objectifPandaList;
     private final Random random;
-    private final int[] objectifPandaList;
 
 
     // Définition des constructeurs
 
     /**
      * Construit la pioche d'objectifs de panda
-     * @param random est un objet Random qui va permettre de créer une pioche aléatoire
+     * @param random un objet Random pour l'aléatoire de la pioche
      */
     public PiocheObjectifPanda(@NotNull Random random) {
-        objectifPandaList = new int[]{5, 4, 3, 3};
+        objectifPandaList = new ArrayList<>(15);
+        creePiocheObjectifsPanda();
+
         this.random = random;
+    }
+
+    /**
+     * Initialise la pioche en lui ajoutant les objectifs de panda
+     */
+    private void creePiocheObjectifsPanda() {
+        for (int i=0; i<5; i++) { // les 5 objectifs de panda vert
+            objectifPandaList.add(new ObjectifPanda(3, 2, Couleur.VERT));
+        }
+        for (int i=0; i<4; i++) { // les 4 objectifs de panda jaune
+            objectifPandaList.add(new ObjectifPanda(4, 2, Couleur.JAUNE));
+        }
+        for (int i=0; i<3; i++) { // les 3 objectifs de panda rose
+            objectifPandaList.add(new ObjectifPanda(5, 2, Couleur.ROSE));
+        }
+        for (int i=0; i<3; i++) { // les 3 objectifs de panda des trois couleurs (à faire)
+            objectifPandaList.add(new ObjectifPanda(6, 3, Couleur.VERT));
+        }
     }
 
 
@@ -35,12 +56,12 @@ public class PiocheObjectifPanda implements PiocheObjectifInterface {
 
     @Override
     public int getNombreObjectifsRestants() {
-        return objectifPandaList[0] + objectifPandaList[1] + objectifPandaList[2] + objectifPandaList[3];
+        return objectifPandaList.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return getNombreObjectifsRestants() == 0;
+        return objectifPandaList.isEmpty();
     }
 
 
@@ -51,42 +72,10 @@ public class PiocheObjectifPanda implements PiocheObjectifInterface {
         assert !isEmpty() : "La pioche d'objectifs de panda est vide";
         int size = getNombreObjectifsRestants();
         int positionCarte = random.nextInt(size);
-        if (positionCarte < 0 || positionCarte >= size) throw new ArithmeticException("Erreur objet random");
-        return prendCarteObjectifPanda(positionCarte);
-    }
-
-    /**
-     * Cherche l'objectif de panda à créer, le renvoie et le retire de la pioche
-     * @param position la position de l'objectif de panda entre toutes les cartes restantes de la pioche
-     * @return l'objectif de panda désigné par la position
-     * @implSpec la position doit être comprise entre 0 et "le nombre de cartes de la pioche - 1"
-     */
-    private ObjectifPanda prendCarteObjectifPanda(int position) {
-        assert position>=0 && position<getNombreObjectifsRestants() : "La position demandée dans la pioche est impossible";
-        int somme = 0;
-        for (int i=0; i<objectifPandaList.length; i++) {
-            somme += objectifPandaList[i];
-            if (position < somme) {
-                objectifPandaList[i]--;
-                return creeCarteObjectifPanda(i);
-            }
+        if (positionCarte < 0 || positionCarte >= size) {
+            throw new ArithmeticException("Erreur objet random");
         }
-        throw new IndexOutOfBoundsException("La position de la carte demandée est en dehors de la pioche");
-    }
-
-    /**
-     * Crée l'objectif de panda désigné par la position et le renvoie
-     * @param indice la position de l'objectif de panda dans le tableau de la pioche
-     * @return l'objectif de panda demandé
-     */
-    private ObjectifPanda creeCarteObjectifPanda(@Range(from = 0, to = 3) int indice) {
-        return switch (indice) {
-            case 0 -> new ObjectifPanda(3, 2, Couleur.VERT);
-            case 1 -> new ObjectifPanda(4, 2, Couleur.VERT);
-            case 2 -> new ObjectifPanda(5, 2, Couleur.ROSE);
-            case 3 -> new ObjectifPanda(6, 3,Couleur.JAUNE);
-            default -> throw new IndexOutOfBoundsException("L'objectif de panda demandé est introuvable");
-        };
+        return objectifPandaList.remove(positionCarte);
     }
 
 
