cdb6c5f1b7f77fe8533cc89565aeeb10d8153be1
amandinemart1 <amandine.martin@etu.unic-cotedazur.fr>
2023-01-22 22:00:29 +0100

    task : modification de la classe Irrigation
    #179

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/pieces/Irrigation.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/pieces/Irrigation.java
@@ -1,11 +1,20 @@
 package fr.cotedazur.univ.polytech.startingpoint.pieces;
 
+import fr.cotedazur.univ.polytech.startingpoint.Position;
+
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Irrigation {
-    Set irrigationDisponible = new HashSet<>();
-    Set irrigationPose = new HashSet<>();
+    List<Position> positions= new ArrayList<>(2);
 
+    public Irrigation(List<Position> positions) {
+        this.positions = positions;
+    }
 
+    public List<Position> getPositions() {
+        return positions;
+    }
 }
