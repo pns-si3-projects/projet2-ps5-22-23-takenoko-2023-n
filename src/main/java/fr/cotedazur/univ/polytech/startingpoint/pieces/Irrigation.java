460b3c3fab5e52d8f73efdb85f8758c6e8378f69
amandinemart1 <amandine.martin@etu.unic-cotedazur.fr>
2023-01-22 21:09:45 +0100

    task : ceration de la classe Irrigation
    #179

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/pieces/Irrigation.java
@@ -0,0 +1,11 @@
+package fr.cotedazur.univ.polytech.startingpoint.pieces;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Irrigation {
+    Set irrigationDisponible = new HashSet<>();
+    Set irrigationPose = new HashSet<>();
+
+
+}
