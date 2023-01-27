9c6b3ebc2f9d15ae249dda5d9514958aa85c9ec7
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-27 18:14:28 +0100

    test: Correction PiocheObjectifJardinier
    #174

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheObjectifJardinierTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheObjectifJardinierTest.java
@@ -1,13 +1,14 @@
 package fr.cotedazur.univ.polytech.startingpoint.pioche;
 
+import fr.cotedazur.univ.polytech.startingpoint.jeu.Couleur;
 import fr.cotedazur.univ.polytech.startingpoint.objectif.ObjectifJardinier;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -43,12 +44,12 @@ class PiocheObjectifJardinierTest {
     void pioche() {
         when(mockRandom.nextInt(anyInt())).thenReturn(14, 10, 11, 0, 1);
         piocheObjectifJardinier = new PiocheObjectifJardinier(mockRandom);
-        Assertions.assertEquals(new ObjectifJardinier(3, 4), piocheObjectifJardinier.pioche());
-        assertEquals(new ObjectifJardinier(5, 4), piocheObjectifJardinier.pioche());
-        // car en supprimant 10, les suivant sont décalés donc on prend ancien 12
-        assertEquals(new ObjectifJardinier(6, 4), piocheObjectifJardinier.pioche());
-        assertEquals(new ObjectifJardinier(5, 4), piocheObjectifJardinier.pioche());
-        // car en supprimant 0, les suivant sont décalés donc on prend ancien 2
-        assertEquals(new ObjectifJardinier(6, 6), piocheObjectifJardinier.pioche());
+        assertEquals(new ObjectifJardinier(3, 4, Couleur.VERTE), piocheObjectifJardinier.pioche());
+        assertEquals(new ObjectifJardinier(5, 4, Couleur.JAUNE), piocheObjectifJardinier.pioche());
+        // car en supprimant 10, les suivants sont décalés donc on prend ancien 12
+        assertEquals(new ObjectifJardinier(6, 4, Couleur.ROSE), piocheObjectifJardinier.pioche());
+        assertEquals(new ObjectifJardinier(5, 4, Couleur.JAUNE), piocheObjectifJardinier.pioche());
+        // car en supprimant 0, les suivants sont décalés donc on prend ancien 2
+        assertEquals(new ObjectifJardinier(6, 6, Couleur.JAUNE), piocheObjectifJardinier.pioche());
     }
 }
