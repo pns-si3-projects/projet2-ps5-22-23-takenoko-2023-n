6d5fa0a2b6db1be9c5487cfd8c24507aa6c5aa2a
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-27 19:15:28 +0100

    test: Correction ObjectifPandaTest
    #174

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/objectif/ObjectifPandaTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/objectif/ObjectifPandaTest.java
@@ -7,46 +7,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectifPandaTest {
-    ObjectifPanda objP1_1;
-    ObjectifPanda objP2_2;
-    ObjectifPanda objP2_2_bis_vert;
-    ObjectifPanda objP2_2_bis_jaune;
     ObjectifPanda objP3_2;
-    ObjectifPanda objP2_3;
+    ObjectifPanda objP3_2Bis;
+    ObjectifPanda objP4_2;
+    ObjectifPanda objP5_2;
+    ObjectifPanda objP6_3;
+    Couleur verte;
+    Couleur rose;
+    Couleur jaune;
 
     @BeforeEach
     void setUp() {
-        objP1_1 = new ObjectifPanda(1, 1,Couleur.ROSE);
-        objP2_2 = new ObjectifPanda(2, 2, Couleur.VERTE);
-        objP2_2_bis_vert = new ObjectifPanda(2, 2,Couleur.VERTE);
-        objP2_2_bis_jaune = new ObjectifPanda(2,2,Couleur.ROSE);
-        objP3_2 = new ObjectifPanda(3, 2,Couleur.JAUNE);
-        objP2_3 = new ObjectifPanda(2, 3,Couleur.JAUNE);
-    }
-
-    @Test
-    void getCouleurObjectifPanda(){
-        assertEquals(Couleur.ROSE,objP1_1.getCouleurBambousAManger());
-        assertNotEquals(Couleur.JAUNE,objP2_2.getCouleurBambousAManger());
-        assertEquals(Couleur.VERTE,objP2_2.getCouleurBambousAManger());
-        assertEquals(Couleur.VERTE,objP2_2_bis_vert.getCouleurBambousAManger());
-        assertEquals(Couleur.JAUNE,objP3_2.getCouleurBambousAManger());
+        verte = Couleur.VERTE;
+        rose = Couleur.ROSE;
+        jaune = Couleur.JAUNE;
+        objP3_2 = new ObjectifPanda(3, 2, verte);
+        objP3_2Bis = new ObjectifPanda(3, 2, verte);
+        objP4_2 = new ObjectifPanda(4, 2, jaune);
+        objP5_2 = new ObjectifPanda(5, 2, rose);
+        objP6_3 = new ObjectifPanda(6, 3, verte);
     }
 
     @Test
     void getNombreBambousAManger() {
-        assertEquals(1, objP1_1.getNombreBambousAManger());
-        assertEquals(2, objP2_2.getNombreBambousAManger());
-        assertEquals(3, objP2_3.getNombreBambousAManger());
-        assertNotEquals(3, objP3_2.getNombreBambousAManger());
+        assertEquals(2, objP3_2.getNombreBambousAManger());
+        assertEquals(2, objP4_2.getNombreBambousAManger());
+        assertEquals(2, objP5_2.getNombreBambousAManger());
+        assertEquals(3, objP6_3.getNombreBambousAManger());
+    }
+
+    @Test
+    void getCouleur(){
+        assertEquals(verte, objP3_2.getCouleur());
+        assertEquals(jaune, objP4_2.getCouleur());
+        assertEquals(rose, objP5_2.getCouleur());
+        assertEquals(verte, objP6_3.getCouleur());
     }
 
     @Test
     void testEquals() {
-        assertEquals(objP2_2, objP2_2_bis_vert);
-        assertNotEquals(objP2_2,objP2_2_bis_jaune);
-        assertNotEquals(objP1_1, objP2_2_bis_vert);
-        assertNotEquals(objP3_2, objP2_2);
-        assertNotEquals(objP2_3, objP2_2);
+        assertEquals(objP3_2, objP3_2Bis);
+        assertNotEquals(objP3_2, objP4_2);
+        assertNotEquals(objP4_2, objP5_2);
+        assertNotEquals(objP5_2, objP6_3);
+        assertNotEquals(objP3_2, objP6_3);
     }
 }
