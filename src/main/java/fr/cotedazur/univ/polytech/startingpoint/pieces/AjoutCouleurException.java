1cbd459caaeded850e76c83c43408cf01a078f3a
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-28 16:23:41 +0100

    task: Correction AjoutCouleurException
    #173

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/pieces/AjoutCouleurException.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/pieces/AjoutCouleurException.java
@@ -1,16 +1,15 @@
 package fr.cotedazur.univ.polytech.startingpoint.pieces;
 
-import fr.cotedazur.univ.polytech.startingpoint.jeu.Couleur;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Exception si la couleur de la section de bambou n'est pas la même que le bambou
+ * Exception si la couleur de la section de bambou n'est pas la même que celle du bambou
  * @author equipe N
  */
-public class AjoutCouleurException extends Exception{
-    public AjoutCouleurException(@NotNull Couleur couleurBambou, @NotNull Couleur couleurSectionBambouAAdd){
-        super("La couleur du bambou (" + couleurBambou
-                + ") n'est pas la même couleur que la section de bambou à ajouter ("
-                + couleurSectionBambouAAdd + ")");
+public class AjoutCouleurException extends Exception {
+    public AjoutCouleurException(@NotNull Bambou bambou, @NotNull SectionBambou sectionBambou){
+        super("La couleur du bambou (" + bambou.getCouleur()
+                + ") n'est pas la même que celle de la section de bambou à ajouter ("
+                + sectionBambou.getCouleur() + ")");
     }
 }
