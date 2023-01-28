f3983051b4b2a1d3a938471954e61627a2bf788c
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-28 11:06:03 +0100

    test: Correction ObjectifTest
    #174

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/objectif/ObjectifTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/objectif/ObjectifTest.java
@@ -7,38 +7,45 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectifTest {
-    Objectif objPar2_2;
-    Objectif objPar3_2;
     Objectif objPar2_3;
-    Objectif objPan2_3;
-    Objectif objPan1_2;
-    Objectif objJar2_1;
+    Objectif objPar3_4;
+    Objectif objPar5_4;
+    Objectif objPan3_2;
+    Objectif objPan6_3;
+    Objectif objJar5_4;
+
 
     @BeforeEach
     void setUp() {
-        objPar2_2 = new ObjectifParcelle(2, 2);
-        objPar3_2 = new ObjectifParcelle(3, 2);
         objPar2_3 = new ObjectifParcelle(2, 3);
-        objPan2_3 = new ObjectifPanda(2, 3, Couleur.VERTE);
-        objPan1_2 = new ObjectifPanda(1, 2,Couleur.VERTE);
-        objJar2_1 = new ObjectifParcelle(2, 1);
+        objPar3_4 = new ObjectifParcelle(3, 4);
+        objPar5_4 = new ObjectifParcelle(5, 4);
+        objPan3_2 = new ObjectifPanda(3, 2, Couleur.VERTE);
+        objPan6_3 = new ObjectifPanda(6, 3, Couleur.VERTE);
+        objJar5_4 = new ObjectifJardinier(5, 4, Couleur.JAUNE);
     }
+
 
     @Test
     void getNombrePoints() {
-        assertEquals(3, objPar3_2.getNombrePoints());
-        assertEquals(1, objPan1_2.getNombrePoints());
-        assertEquals(2, objJar2_1.getNombrePoints());
-        assertNotEquals(3, objPar2_3.getNombrePoints());
+        assertEquals(2, objPar2_3.getNombrePoints());
+        assertEquals(3, objPar3_4.getNombrePoints());
+        assertEquals(5, objPar5_4.getNombrePoints());
+        assertEquals(3, objPan3_2.getNombrePoints());
+        assertEquals(6, objPan6_3.getNombrePoints());
+        assertEquals(5, objJar5_4.getNombrePoints());
     }
+
 
     /* Je n'arrive pas à utiliser le equals de Objectif --> prend automatiquement celui de la sous-classe
     @Test
     void testEquals() {
-        assertTrue(objPar2_2.equals(objPar2_3));
-        assertEquals(objPar2_2, objPan2_3);
-        assertEquals(objPan2_3, objJar2_1);
-        assertNotEquals(objPan1_2, objJar2_1);
-        assertNotEquals(objPar2_2, objPar2_3);
+        assertNotEquals(objPar2_3, objPar3_4);
+        assertNotEquals(objPar3_4, objPar5_4);
+        assertNotEquals(objPar5_4, objPan3_2);
+        assertNotEquals(objPan3_2, objPan6_3);
+        assertNotEquals(objPan6_3, objJar5_4);
+        assertEquals(objPar3_4, objPan3_2);
+        assertEquals(objPar5_4, objJar5_4);
     }*/
 }
