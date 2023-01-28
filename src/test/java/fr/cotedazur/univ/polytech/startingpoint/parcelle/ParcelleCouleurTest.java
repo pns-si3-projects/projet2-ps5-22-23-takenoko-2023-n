736ea5c84f601795975d136fc347f7b513825cd3
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-28 15:18:42 +0100

    test: Correction ParcelleCouleurTest
    #174

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/parcelle/ParcelleCouleurTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/parcelle/ParcelleCouleurTest.java
@@ -8,44 +8,54 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParcelleCouleurTest {
-    Couleur vert = Couleur.VERTE;
-    Couleur rose = Couleur.ROSE;
-    Couleur jaune = Couleur.JAUNE;
     ParcelleCouleur pC1_1V;
     ParcelleCouleur pC1_1VBis;
     ParcelleCouleur pC1_1R;
     ParcelleCouleur pC1_1J;
-    ParcelleCouleur pC2_1R;
+    ParcelleCouleur pC2_0R;
     Position p1_1;
-    Position p2_1;
+    Position p2_0;
+    Couleur verte;
+    Couleur rose;
+    Couleur jaune;
+
 
     @BeforeEach
     void setUp() {
         p1_1 = new Position(1,1);
-        p2_1 = new Position(2,1);
-        pC1_1V = new ParcelleCouleur(p1_1, vert);
-        pC1_1VBis = new ParcelleCouleur(p1_1, vert);
+        p2_0 = new Position(2,0);
+        verte = Couleur.VERTE;
+        rose = Couleur.ROSE;
+        jaune = Couleur.JAUNE;
+        pC1_1V = new ParcelleCouleur(p1_1, verte);
+        pC1_1VBis = new ParcelleCouleur(p1_1, verte);
         pC1_1R = new ParcelleCouleur(p1_1, rose);
         pC1_1J = new ParcelleCouleur(p1_1, jaune);
-        pC2_1R = new ParcelleCouleur(p2_1, rose);
+        pC2_0R = new ParcelleCouleur(p2_0, rose);
     }
 
+
     @Test
-    void position() {
+    void getPosition() {
         assertEquals(p1_1, pC1_1V.getPosition());
-        assertEquals(p1_1, pC1_1VBis.getPosition());
-        assertEquals(pC1_1V.getPosition(), pC1_1VBis.getPosition());
-        assertEquals(p2_1, pC2_1R.getPosition());
+        assertEquals(p1_1, pC1_1R.getPosition());
+        assertEquals(p1_1, pC1_1J.getPosition());
+        assertEquals(p2_0, pC2_0R.getPosition());
     }
 
     @Test
-    void couleur() {
-        assertEquals(vert, pC1_1V.getCouleur());
-        assertEquals(vert, pC1_1VBis.getCouleur());
-        assertEquals(pC1_1V.getCouleur(), pC1_1VBis.getCouleur());
+    void getCouleur() {
+        assertEquals(verte, pC1_1V.getCouleur());
         assertEquals(rose, pC1_1R.getCouleur());
         assertEquals(jaune, pC1_1J.getCouleur());
-        assertEquals(rose, pC2_1R.getCouleur());
+        assertEquals(rose, pC2_0R.getCouleur());
+    }
+
+
+    @Test
+    void testToString() {
+        assertEquals("Parcelle de couleur verte en (1,1)", pC1_1V.toString());
+        assertEquals("Parcelle de couleur rose en (2,0)", pC2_0R.toString());
     }
 
     @Test
@@ -54,7 +64,6 @@ class ParcelleCouleurTest {
         assertNotEquals(pC1_1V, pC1_1R);
         assertNotEquals(pC1_1V, pC1_1J);
         assertNotEquals(pC1_1R, pC1_1J);
-        assertNotEquals(pC1_1V, pC2_1R);
-        assertNotEquals(pC1_1VBis, pC2_1R);
+        assertNotEquals(pC1_1V, pC2_0R);
     }
 }
