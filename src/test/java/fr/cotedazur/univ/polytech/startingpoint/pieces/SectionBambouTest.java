52162e92b0e1f15764aadd7a1d5eaacc3a608ee8
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-28 16:26:44 +0100

    test: Correction SectionBambousTest
    #174

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/pieces/SectionBambouTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/pieces/SectionBambouTest.java
@@ -6,36 +6,48 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SectionBambouTest {
-    SectionBambou bambouVert;
-    SectionBambou bambouJaune;
-    SectionBambou bambouRose;
-    SectionBambou bambouVertBis;
+    SectionBambou sectionBambouVert;
+    SectionBambou sectionBambouVertBis;
+    SectionBambou sectionbambouRose;
+    SectionBambou sectionbambouJaune;
+    Couleur verte;
+    Couleur rose;
+    Couleur jaune;
+
 
     @BeforeEach
     void setUp() {
-        bambouVert = new SectionBambou(Couleur.VERTE);
-        bambouJaune = new SectionBambou(Couleur.JAUNE);
-        bambouRose = new SectionBambou(Couleur.ROSE);
-        bambouVertBis = new SectionBambou(Couleur.VERTE);
+        verte = Couleur.VERTE;
+        rose = Couleur.ROSE;
+        jaune = Couleur.JAUNE;
+        sectionBambouVert = new SectionBambou(verte);
+        sectionBambouVertBis = new SectionBambou(verte);
+        sectionbambouRose = new SectionBambou(rose);
+        sectionbambouJaune = new SectionBambou(jaune);
     }
+
 
     @Test
     void getCouleur() {
-        assertEquals(Couleur.VERTE,bambouVert.getCouleur());
-        assertNotEquals(Couleur.JAUNE,bambouRose.getCouleur());
-        assertEquals(Couleur.ROSE,bambouRose.getCouleur());
-        assertEquals(Couleur.JAUNE,bambouJaune.getCouleur());
-        assertEquals(Couleur.VERTE,bambouVertBis.getCouleur());
+        assertEquals(verte, sectionBambouVert.getCouleur());
+        assertEquals(rose, sectionbambouRose.getCouleur());
+        assertEquals(jaune, sectionbambouJaune.getCouleur());
+    }
+
+
+    @Test
+    void testToString() {
+        assertEquals("Section de bambou de couleur verte", sectionBambouVert.toString());
+        assertEquals("Section de bambou de couleur rose", sectionbambouRose.toString());
+        assertEquals("Section de bambou de couleur jaune", sectionbambouJaune.toString());
     }
 
     @Test
     void testEquals() {
-        assertEquals(bambouVert,bambouVertBis);
-        assertNotEquals(bambouVert,bambouJaune);
-        assertNotEquals(bambouVert,bambouRose);
-        assertThrows(IllegalArgumentException.class, () -> {new SectionBambou(null);});
+        assertEquals(sectionBambouVert, sectionBambouVertBis);
+        assertNotEquals(sectionBambouVert, sectionbambouRose);
+        assertNotEquals(sectionbambouRose, sectionbambouJaune);
     }
 }
