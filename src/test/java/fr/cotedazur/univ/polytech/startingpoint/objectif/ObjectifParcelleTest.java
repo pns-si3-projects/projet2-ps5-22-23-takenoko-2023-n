0a43fb43bae12ef8186643c91ff04ca719e45f80
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-28 10:41:20 +0100

    test: Correction ObjectifParcelleTest
    #174

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/objectif/ObjectifParcelleTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/objectif/ObjectifParcelleTest.java
@@ -1,6 +1,5 @@
 package fr.cotedazur.univ.polytech.startingpoint.objectif;
 
-import fr.cotedazur.univ.polytech.startingpoint.objectif.ObjectifParcelle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,34 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ObjectifParcelleTest {
-    ObjectifParcelle objP1_1;
-    ObjectifParcelle objP2_2;
-    ObjectifParcelle objP2_2_bis;
-    ObjectifParcelle objP3_2;
     ObjectifParcelle objP2_3;
+    ObjectifParcelle objP2_3Bis;
+    ObjectifParcelle objP3_4;
+    ObjectifParcelle objP4_3;
+
 
     @BeforeEach
     void setUp() {
-        objP1_1 = new ObjectifParcelle(1, 1);
-        objP2_2 = new ObjectifParcelle(2, 2);
-        objP2_2_bis = new ObjectifParcelle(2, 2);
-        objP3_2 = new ObjectifParcelle(3, 2);
         objP2_3 = new ObjectifParcelle(2, 3);
+        objP2_3Bis = new ObjectifParcelle(2, 3);
+        objP3_4 = new ObjectifParcelle(3, 4);
+        objP4_3 = new ObjectifParcelle(4, 3);
     }
 
+
     @Test
-    void getNombreParcelles() {
-        assertEquals(1, objP1_1.getNombreParcelles());
-        assertEquals(2, objP2_2.getNombreParcelles());
-        assertEquals(3, objP2_3.getNombreParcelles());
-        assertNotEquals(3, objP3_2.getNombreParcelles());
+    void getSchema() {
+        assertEquals(3, objP2_3.getSchema());
+        assertEquals(4, objP3_4.getSchema());
+        assertEquals(3, objP4_3.getSchema());
     }
+
 
     @Test
     void testEquals() {
-        assertEquals(objP2_2, objP2_2_bis);
-        assertNotEquals(objP1_1, objP2_2_bis);
-        assertNotEquals(objP3_2, objP2_2);
-        assertNotEquals(objP2_3, objP2_2);
+        assertEquals(objP2_3, objP2_3Bis);
+        assertNotEquals(objP2_3, objP3_4);
+        assertNotEquals(objP3_4, objP4_3);
+        assertNotEquals(objP2_3, objP4_3);
     }
 }
