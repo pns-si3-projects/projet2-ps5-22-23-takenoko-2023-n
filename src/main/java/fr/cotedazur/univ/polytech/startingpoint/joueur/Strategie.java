0f7b12a7566bf9c8629bc5b4d33860bef461bbe2
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-01-31 19:08:07 +0100

    task: Création interface Strategie
    #189

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/Strategie.java
@@ -0,0 +1,12 @@
+package fr.cotedazur.univ.polytech.startingpoint.joueur;
+
+/**
+ * Permet au joueur d'appliquer une stratégie de jeu
+ * @author equipe N
+ */
+public interface Strategie {
+    /** L'ensemble des stratégies possible pour le joueur */
+    enum StrategiePossible {PARCELLE, JARDINIER, PANDA}
+
+    
+}
