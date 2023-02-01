70849264d445cf28db5c597a6f361107946804af
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-01 16:04:52 +0100

    task: Création StrategieJardinier
    #189

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/StrategieJardinier.java
@@ -0,0 +1,29 @@
+package fr.cotedazur.univ.polytech.startingpoint.joueur;
+
+import fr.cotedazur.univ.polytech.startingpoint.objectif.Objectif;
+
+import java.util.List;
+
+public class StrategieJardinier implements Strategie {
+    // Méthodes d'utilisation
+
+    @Override
+    public Plaquette.ActionPossible choisiActionTour(boolean[] actionsRealiseesTour, List<Objectif> objectifs) {
+        Plaquette.ActionPossible jardinier = Plaquette.ActionPossible.JARDINIER;
+        if (!actionsRealiseesTour[jardinier.ordinal()]) {
+            return jardinier;
+        }
+
+        Plaquette.ActionPossible objectif = Plaquette.ActionPossible.OBJECTIF;
+        if (!actionsRealiseesTour[objectif.ordinal()]) {
+            return objectif;
+        }
+
+        Plaquette.ActionPossible parcelle = Plaquette.ActionPossible.PARCELLE;
+        if (!actionsRealiseesTour[parcelle.ordinal()]) {
+            return parcelle;
+        }
+
+        return Plaquette.ActionPossible.PANDA;
+    }
+}
