2131f67c3ee3f1ba2991d47fc76a7be0f0d3cbb0
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-03 23:02:34 +0100

    task: Ajout commentaire documentation classe GestionParcelles
    #191

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/plateau/GestionParcelles.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/plateau/GestionParcelles.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Permet de gérer les parcelles du plateau
+ */
 public class GestionParcelles {
     // Définition des attributs
 
