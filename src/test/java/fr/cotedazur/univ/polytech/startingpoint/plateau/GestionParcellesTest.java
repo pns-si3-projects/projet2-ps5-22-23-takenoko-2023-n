040b8e75b5984989b6f00c61f7f2a332ba279e53
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-03 14:01:08 +0100

    test: Correction méthodes ajout parcelles GestionParcellesTest
    #194

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/plateau/GestionParcellesTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/plateau/GestionParcellesTest.java
@@ -6,6 +6,7 @@ import fr.cotedazur.univ.polytech.startingpoint.parcelle.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -59,19 +60,84 @@ class GestionParcellesTest {
     @Test
     void futuresVoisines() {
         assertThrows(ParcelleExistanteException.class, () -> GestionParcelles.futuresVoisines(parcelles, etang));
+        assertThrows(ParcelleExistanteException.class, () -> GestionParcelles.futuresVoisines(parcelles, pC1_m1J));
 
+        Parcelle pCm1_m1V = new ParcelleCouleur(new Position(-1, 1), verte);
+        List<Parcelle> futuresVoisines;
         try {
-            Parcelle[] futuresVoisines =
-                    GestionParcelles.futuresVoisines(parcelles, new ParcelleCouleur(new Position(-1, 1), jaune));
-
-            assertEquals(new ParcelleDisponible(new Position(0, 2)), futuresVoisines[0]);
-            assertEquals(pC1_1R, futuresVoisines[1]);
-            assertEquals(etang, futuresVoisines[2]);
-            assertEquals(new ParcelleDisponible(new Position(-2, 0)), futuresVoisines[3]);
-            assertEquals(new ParcelleDisponible(new Position(-3, 1)), futuresVoisines[4]);
-            assertEquals(new ParcelleDisponible(new Position(-2, 2)), futuresVoisines[5]);
+            futuresVoisines = GestionParcelles.futuresVoisines(parcelles, pCm1_m1V);
         } catch (ParcelleExistanteException e) {
             throw new AssertionError(e);
         }
+
+        assertTrue(futuresVoisines.contains(etang));
+        assertTrue(futuresVoisines.contains(pC1_1R));
+    }
+
+    @Test
+    void indiceVoisine() {
+        Parcelle pDm1_1 = new ParcelleDisponible(new Position(-1, 1));
+        Parcelle pD3_1 = new ParcelleDisponible(new Position(3, 1));
+        Parcelle pD4_0 = new ParcelleDisponible(new Position(4, 0));
+
+        assertEquals(-1, GestionParcelles.indiceVoisine(pC2_0V, pDm1_1));
+        assertEquals(0, GestionParcelles.indiceVoisine(pC2_0V, pD3_1));
+        assertEquals(1, GestionParcelles.indiceVoisine(pC2_0V, pD4_0));
+        assertEquals(2, GestionParcelles.indiceVoisine(pC2_0V, pC3_m1R));
+        assertEquals(3, GestionParcelles.indiceVoisine(pC2_0V, pC1_m1J));
+        assertEquals(4, GestionParcelles.indiceVoisine(pC2_0V, etang));
+        assertEquals(5, GestionParcelles.indiceVoisine(pC2_0V, pC1_1R));
+    }
+
+    @Test
+    void ajouteVoisinesDisponibles() {
+        Parcelle pCm1_m1V = new ParcelleCouleur(new Position(-1, 1), verte);
+        List<Parcelle> futuresVoisines;
+        try {
+            futuresVoisines = GestionParcelles.futuresVoisines(parcelles, pCm1_m1V);
+        } catch (ParcelleExistanteException e) {
+            throw new AssertionError(e);
+        }
+
+        Parcelle[] voisines = GestionParcelles.ajouteVoisinesDisponibles(futuresVoisines, pCm1_m1V);
+        Parcelle pD0_2 = new ParcelleDisponible(new Position(0, 2));
+        Parcelle pDm2_0 = new ParcelleDisponible(new Position(-2, 0));
+        Parcelle pDm3_1 = new ParcelleDisponible(new Position(-3, 1));
+        Parcelle pDm2_2 = new ParcelleDisponible(new Position(-2, 2));
+
+        assertEquals(pD0_2, voisines[0]);
+        assertEquals(pC1_1R, voisines[1]);
+        assertEquals(etang, voisines[2]);
+        assertEquals(pDm2_0, voisines[3]);
+        assertEquals(pDm3_1, voisines[4]);
+        assertEquals(pDm2_2, voisines[5]);
+    }
+
+    @Test
+    void positionsDisponibles() {
+        Parcelle pCm1_m1V = new ParcelleCouleur(new Position(-1, 1), verte);
+        List<Parcelle> futuresVoisines;
+        try {
+            futuresVoisines = GestionParcelles.futuresVoisines(parcelles, pCm1_m1V);
+        } catch (ParcelleExistanteException e) {
+            throw new AssertionError(e);
+        }
+        Parcelle[] voisines = GestionParcelles.ajouteVoisinesDisponibles(futuresVoisines, pCm1_m1V);
+
+        Parcelle[] parcellesPlusPCm1_m1V = new Parcelle[6];
+        System.arraycopy(parcelles, 0, parcellesPlusPCm1_m1V, 0, 5);
+        parcellesPlusPCm1_m1V[5] = pCm1_m1V;
+
+        Position p0_2 = new Position(0, 2);
+        Position pm2_0 = new Position(-2, 0);
+        Position pm3_1 = new Position(-3, 1);
+        Position pm2_2 = new Position(-2, 2);
+
+        List<Position> nouvellesPDis = GestionParcelles.positionsDisponibles(parcellesPlusPCm1_m1V, voisines);
+
+        assertTrue(nouvellesPDis.contains(p0_2));
+        assertTrue(nouvellesPDis.contains(pm2_0));
+        assertFalse(nouvellesPDis.contains(pm3_1));
+        assertFalse(nouvellesPDis.contains(pm2_2));
     }
 }
