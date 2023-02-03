429edb1bd7149d73788e05b30b0cfcf8f19f71ba
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-03 13:59:24 +0100

    task: Méthodes d'ajout de parcelle GestionParcelles
    #191

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/plateau/GestionParcelles.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/plateau/GestionParcelles.java
@@ -1,11 +1,14 @@
 package fr.cotedazur.univ.polytech.startingpoint.plateau;
 
 import fr.cotedazur.univ.polytech.startingpoint.jeu.Position;
+import fr.cotedazur.univ.polytech.startingpoint.parcelle.Etang;
 import fr.cotedazur.univ.polytech.startingpoint.parcelle.Parcelle;
 import fr.cotedazur.univ.polytech.startingpoint.parcelle.ParcelleDisponible;
 import fr.cotedazur.univ.polytech.startingpoint.parcelle.ParcelleExistanteException;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class GestionParcelles {
@@ -34,44 +37,36 @@ public class GestionParcelles {
     }
 
     /**
-     * Renvoie les futures voisines de la parcelle ciblée
-     * @param parcelles le tableau des parcelles du plateau
+     * Renvoie les futures voisines déjà posées de la parcelle ciblée
+     * @param parcelles le tableau des parcelles posées
      * @param parcelle la parcelle cible
-     * @return un tableau des futures voisines de la parcelle ciblée (contenant des ParcelleDisponible)
+     * @return la liste des futures voisines de la parcelle ciblée
      */
-    public static Parcelle[] futuresVoisines(@NotNull Parcelle[] parcelles, @NotNull Parcelle parcelle)
+    public static List<Parcelle> futuresVoisines(@NotNull Parcelle[] parcelles, @NotNull Parcelle parcelle)
             throws ParcelleExistanteException {
 
         if (chercheParcelle(parcelles, parcelle.getPosition()).isPresent()) {
             throw new ParcelleExistanteException(parcelle);
         }
-        Parcelle[] futuresVoisinesParcelle = chercheFuturesVoisines(parcelles, parcelle);
 
-        for (int i=0; i<6; i++) {
-            if (futuresVoisinesParcelle[i] == null) {
-                futuresVoisinesParcelle[i] = creeVoisineDisponible(i, parcelle.getPosition());
-            }
-        }
-
-        return futuresVoisinesParcelle;
+        return chercheFuturesVoisines(parcelles, parcelle);
     }
 
     /**
      * Renvoie les parcelles qui seront voisines de la parcelle ciblée
-     * @param parcelles le tableau des parcelles du plateau
+     * @param parcelles le tableau des parcelles posées
      * @param parcelle la parcelle ciblée
-     * @return un tableau trié des futures voisines de la parcelle ciblée
+     * @return la liste des futures voisines de la parcelle ciblée
      */
-    private static Parcelle[] chercheFuturesVoisines(Parcelle[] parcelles, Parcelle parcelle) {
-        Parcelle[] voisinesParcelle = new Parcelle[6];
+    private static List<Parcelle> chercheFuturesVoisines(Parcelle[] parcelles, Parcelle parcelle) {
+        List<Parcelle> voisinesParcelle = new ArrayList<>();
 
         for (Parcelle possibleVoisineParcelle : parcelles) {
             Position posPar = parcelle.getPosition();
             Position posVoi = possibleVoisineParcelle.getPosition();
 
             if (estFutureVoisine(posPar, posVoi)) {
-                int indiceVoisineParcelle = indiceVoisine(posPar, posVoi);
-                voisinesParcelle[indiceVoisineParcelle] = possibleVoisineParcelle;
+                voisinesParcelle.add(possibleVoisineParcelle);
             }
         }
         return voisinesParcelle;
@@ -94,16 +89,16 @@ public class GestionParcelles {
     }
 
     /**
-     * Renvoie la direction d'une voisine par rapport à la parcelle ciblée
-     * @param positionParcelle la position de la parcelle ciblée
-     * @param positionVoisine la position de la voisine de la parcelle ciblée
-     * @return la direction d'une voisine par rapport à la parcelle ciblée
+     * Renvoie la direction de la voisine par rapport à la parcelle ciblée
+     * @param parcelle la parcelle ciblée
+     * @param voisine la voisine de la parcelle ciblée
+     * @return la direction de la voisine par rapport à la parcelle ciblée
      */
-    private static int indiceVoisine(Position positionParcelle, Position positionVoisine) {
-        int xC = positionParcelle.getX();
-        int yC = positionParcelle.getY();
-        int xV = positionVoisine.getX();
-        int yV = positionVoisine.getY();
+    public static int indiceVoisine(Parcelle parcelle, Parcelle voisine) {
+        int xC = parcelle.getPosition().getX();
+        int yC = parcelle.getPosition().getY();
+        int xV = voisine.getPosition().getX();
+        int yV = voisine.getPosition().getY();
 
         if (yV == yC) { // Si proche et sur la même ligne
             if (xV == xC + 2) return 1;
@@ -118,6 +113,28 @@ public class GestionParcelles {
             else if (xV == xC - 1) return 3;
         }
         return -1;
+    }
+
+    /**
+     * Permet de compléter les voisines de la parcelle ciblée par des ParcelleDisponible
+     * @param voisinesPosees la liste des voisines déjà posées de la parcelle ciblée
+     * @param parcelle la parcelle ciblée
+     * @return un tableau des voisines de la parcelle ciblée (complété par des ParcelleDisponible)
+     */
+    public static Parcelle[] ajouteVoisinesDisponibles(List<Parcelle> voisinesPosees, Parcelle parcelle) {
+        Parcelle[] voisinesParcelle = new Parcelle[6];
+
+        for (Parcelle voisine : voisinesPosees) {
+            int indiceVoisine = indiceVoisine(parcelle, voisine);
+            voisinesParcelle[indiceVoisine] = voisine;
+        }
+
+        for (int i=0; i<6; i++) {
+            if (voisinesParcelle[i] == null) {
+                voisinesParcelle[i] = creeVoisineDisponible(i, parcelle.getPosition());
+            }
+        }
+        return voisinesParcelle;
     }
 
     /**
@@ -139,5 +156,41 @@ public class GestionParcelles {
             case 5 -> new ParcelleDisponible(new Position(x-1, y+1));
             default -> throw new IllegalArgumentException("Indice de direction incorrecte");
         };
+    }
+
+    /**
+     * Renvoie les nouvelles positions disponibles
+     * @param parcelles le tableau des parcelles posées
+     * @param voisines le tableau des voisines de la dernière parcelle posée
+     * @return la liste des nouvelles positions disponibles
+     */
+    public static List<Position> positionsDisponibles(Parcelle[] parcelles, Parcelle[] voisines) {
+        List<Position> positionsDisponibles = new ArrayList<>();
+
+        for (Parcelle voisine : voisines) {
+            if (voisine instanceof ParcelleDisponible parcelleDisponible
+                    && estPositionDisponible(parcelles, parcelleDisponible)) {
+
+                positionsDisponibles.add(voisine.getPosition());
+            }
+        }
+
+        return positionsDisponibles;
+    }
+
+    /**
+     * Renvoie si la position de la parcelle disponible est une position possible pour poser une parcelle
+     * @param parcelles le tableau des parcelles posées
+     * @param parcelleDisponible la parcelle disponible ciblée
+     * @return {@code true} si la position de la parcelle disponible est posable
+     */
+    private static boolean estPositionDisponible(Parcelle[] parcelles, ParcelleDisponible parcelleDisponible) {
+        List<Parcelle> parcellesVoisines = GestionParcelles.chercheFuturesVoisines(parcelles, parcelleDisponible);
+
+        if (parcellesVoisines.contains(new Etang())) {
+            return true;
+        }
+        int nbVoisines = parcellesVoisines.size();
+        return nbVoisines >= 2 && nbVoisines < 6;
     }
 }
