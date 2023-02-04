472e2461abbe5814995e635207b77b66ca334a99
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-04 19:20:30 +0100

    task: ajout méthodes victoire Afficheur
    #192

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Afficheur.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Afficheur.java
@@ -1,5 +1,7 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
+import fr.cotedazur.univ.polytech.startingpoint.joueur.Joueur;
+
 /**
  * Affiche les changements dans le jeu.
  * @author equipe N
@@ -10,5 +12,21 @@ public class Afficheur {
      */
     public static void initialisation() {
         System.out.println("Initialisation du jeu : étang, panda et jardinier en (0,0).");
+    }
+
+    /**
+     * Affiche la victoire du joueur gagnant
+     * @param joueur le joueur gagnant
+     */
+    public static void victoire(Joueur joueur) {
+        System.out.println("\n\nBravo à " + joueur.getNom() + " d'avoir remporté la partie avec "
+            + joueur.nombreObjectifsTermines() + " objectifs terminés pour " + joueur.nombrePoints() + " points !");
+    }
+
+    /**
+     * Affiche le partage de la victoire par tous les joueurs
+     */
+    public static void victoire() {
+        System.out.println("\n\nBravo à tous les joueurs d'avoir participé et à bientôt pour une autre partie !");
     }
 }
