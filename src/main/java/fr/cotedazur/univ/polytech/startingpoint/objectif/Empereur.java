6858589a3a1266326c1bff9cb5159bc1055d78a7
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-04 18:44:48 +0100

    task: Création carte Empereur
    #192

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/objectif/Empereur.java
@@ -0,0 +1,12 @@
+package fr.cotedazur.univ.polytech.startingpoint.objectif;
+
+public class Empereur extends Objectif {
+    // Définition des constructeurs
+
+    /**
+     * Construit la carte Empereur par son nombre de points
+     */
+    public Empereur() {
+        nombrePoints = 2;
+    }
+}
