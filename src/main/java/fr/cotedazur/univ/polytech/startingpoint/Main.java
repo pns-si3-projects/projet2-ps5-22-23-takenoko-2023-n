c2e257db270ad13065e30c603f07d6cfd0461e1e
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-04 19:21:17 +0100

    task: Ajout méthode jeu et correction contructeur MaitreDuJeu
    #192

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -1,10 +1,15 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
 import fr.cotedazur.univ.polytech.startingpoint.jeu.MaitreDuJeu;
+import fr.cotedazur.univ.polytech.startingpoint.joueur.Joueur;
+import fr.cotedazur.univ.polytech.startingpoint.joueur.Strategie;
 
 public class Main {
 
     public static void main(String... args) {
-        MaitreDuJeu maitreDuJeu = new MaitreDuJeu("Joueur1", "Joueur2");
+        Joueur joueur1 = new Joueur("joueur1", Strategie.StrategiePossible.PARCELLE);
+        Joueur joueur2 = new Joueur("joueur2", Strategie.StrategiePossible.JARDINIER);
+        Joueur joueur3 = new Joueur("joueur3", Strategie.StrategiePossible.PANDA);
+        MaitreDuJeu maitreDuJeu = new MaitreDuJeu(joueur1, joueur2, joueur3);
     }
 }
--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/jeu/MaitreDuJeu.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/jeu/MaitreDuJeu.java
@@ -2,10 +2,13 @@ package fr.cotedazur.univ.polytech.startingpoint.jeu;
 
 import fr.cotedazur.univ.polytech.startingpoint.Afficheur;
 import fr.cotedazur.univ.polytech.startingpoint.joueur.Joueur;
-import fr.cotedazur.univ.polytech.startingpoint.joueur.Strategie;
+import fr.cotedazur.univ.polytech.startingpoint.objectif.Empereur;
 import fr.cotedazur.univ.polytech.startingpoint.pioche.*;
 import fr.cotedazur.univ.polytech.startingpoint.plateau.Plateau;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.Random;
 
 /**
@@ -21,22 +24,65 @@ public class MaitreDuJeu {
     public static final PiocheObjectifPanda PIOCHE_OBJECTIF_PANDA = new PiocheObjectifPanda(new Random());
     public static final PiocheObjectifJardinier PIOCHE_OBJECTIF_JARDINIER = new PiocheObjectifJardinier(new Random());
     public static final PiocheSectionBambou PIOCHE_SECTION_BAMBOU = new PiocheSectionBambou(new Random());
+    private final GestionTours gestionTours;
     private final Joueur[] joueurs;
-    private int nombreTour;
+    /** Le nombre d'objectifs demandé pour recevoir la carte empereur */
+    private final int nombreObjectifsDemandes;
 
 
     // Définition des constructeurs
 
     /**
      * Initialise le jeu (joueurs, plateau, pioches)
+     * @param joueurs les joueurs de la partie, entre 2 et 4 joueurs
      */
-    public MaitreDuJeu(String nomJoueur1, String nomJoueur2) {
-        Joueur joueur1 = new Joueur(nomJoueur1, Strategie.StrategiePossible.PARCELLE);
-        Joueur joueur2 = new Joueur(nomJoueur2, Strategie.StrategiePossible.PANDA);
+    public MaitreDuJeu(@NotNull Joueur ...joueurs) {
+        int nbJoueurs = joueurs.length;
+        if (nbJoueurs < 2 || nbJoueurs > 4) {
+            throw new IllegalArgumentException(nbJoueurs < 2 ? "Il n'y a pas assez de joueurs" : "Il y a trop de joueurs");
+        }
 
-        joueurs = new Joueur[]{joueur1, joueur2};
-        nombreTour = 1;
+        this.joueurs = Arrays.copyOf(joueurs, nbJoueurs);
+        gestionTours = new GestionTours();
+        nombreObjectifsDemandes = nombreObjectifsDemandes(joueurs.length);
 
         Afficheur.initialisation();
+    }
+
+    /**
+     * Calcul le nombre d'objectifs demandé pour recevoir la carte empereur
+     * @param nbJoueurs le nombre de joueurs de la partie
+     * @return le nombre d'objectifs demandé
+     */
+    private int nombreObjectifsDemandes(int nbJoueurs) {
+        return switch (nbJoueurs) {
+            case 2 -> 9;
+            case 3 -> 8;
+            case 4 -> 7;
+            default -> {
+                assert false : "Nombre de joueurs impossible";
+                yield 0;
+            }
+        };
+    }
+
+
+    // Méthodes d'utilisation
+
+    /**
+     * Lance la partie du jeu et donne les tours aux joueurs
+     */
+    public void jeu() {
+        Joueur joueurFinObjectifs = gestionTours.tours(joueurs, nombreObjectifsDemandes);
+        joueurFinObjectifs.recoitEmpereur(new Empereur());
+
+        gestionTours.dernierTour(joueurs, joueurFinObjectifs);
+
+        Optional<Joueur> joueurOpt = gestionTours.gagnant(joueurs);
+        if (joueurOpt.isPresent()) {
+            Afficheur.victoire(joueurOpt.get());
+        } else {
+            Afficheur.victoire();
+        }
     }
 }
