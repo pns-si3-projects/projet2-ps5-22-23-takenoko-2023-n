5b510e058e4e324a15501b40670ce32334f4e42b
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-02-05 15:09:05 +0100

    task: Ajout des nouveaux motifs dans piocheObjectifParcelle
    Ajout du motif Decale et motif Losange dans la Pioche ObjectifParcelle #216

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheObjectifParcelle.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheObjectifParcelle.java
@@ -3,7 +3,9 @@ package fr.cotedazur.univ.polytech.startingpoint.pioche;
 import fr.cotedazur.univ.polytech.startingpoint.Couleur;
 import fr.cotedazur.univ.polytech.startingpoint.Position;
 import fr.cotedazur.univ.polytech.startingpoint.motif.Motif;
+import fr.cotedazur.univ.polytech.startingpoint.motif.MotifDecale;
 import fr.cotedazur.univ.polytech.startingpoint.motif.MotifDiagonale;
+import fr.cotedazur.univ.polytech.startingpoint.motif.MotifLosange;
 import fr.cotedazur.univ.polytech.startingpoint.objectif.ObjectifParcelle;
 import fr.cotedazur.univ.polytech.startingpoint.parcelle.ParcelleCouleur;
 
@@ -17,8 +19,11 @@ import java.util.Random;
 public class PiocheObjectifParcelle extends ArrayList<ObjectifParcelle> implements PiocheObjectifInterface {
     // Définition des attributs
     private final Random random;
-    private Motif motifParDefaut2Parcelles;
-    private Motif motifParDefaut3Parcelles;
+    private Motif motifDiagonale2Parcelles;
+    private Motif motifDiagonale3Parcelles;
+    private Motif motifLosange;
+
+    private Motif motifDecale;
 
 
     // Définition des constructeurs
@@ -27,8 +32,15 @@ public class PiocheObjectifParcelle extends ArrayList<ObjectifParcelle> implemen
      * @param random est un objet Random qui va permettre de créer une pioche aléatoire
      */
     public PiocheObjectifParcelle(Random random) {
-        motifParDefaut2Parcelles = new MotifDiagonale(new ParcelleCouleur(new Position(0,0), Couleur.VERT), new ParcelleCouleur(new Position(1,1), Couleur.VERT));
-        motifParDefaut3Parcelles = new MotifDiagonale(new ParcelleCouleur(new Position(-1,-1), Couleur.VERT), new ParcelleCouleur(new Position(0,0), Couleur.VERT), new ParcelleCouleur(new Position(1,1),Couleur.VERT));
+        ParcelleCouleur parcelleCouleur00 = new ParcelleCouleur(new Position(0,0), Couleur.VERT);
+        ParcelleCouleur parcelleCouleur11 = new ParcelleCouleur(new Position(1,1), Couleur.VERT);
+        ParcelleCouleur parcelleCouleurm11 = new ParcelleCouleur(new Position(-1, 1), Couleur.VERT);
+        ParcelleCouleur parcelleCouleur02 = new ParcelleCouleur(new Position(0, 2), Couleur.VERT);
+        ParcelleCouleur parcelleCouleurm1m1 = new ParcelleCouleur(new Position(-1,-1), Couleur.VERT);
+        motifDiagonale2Parcelles = new MotifDiagonale(parcelleCouleur00, parcelleCouleur11);
+        motifDiagonale3Parcelles = new MotifDiagonale(parcelleCouleurm1m1, parcelleCouleur00, parcelleCouleur11);
+        motifLosange = new MotifLosange(parcelleCouleur00, parcelleCouleur11, parcelleCouleurm11, parcelleCouleur02);
+        motifDecale = new MotifDecale(parcelleCouleur00, parcelleCouleur11, parcelleCouleur02);
         creePiocheObjectifsParcelle();
         this.random = random;
     }
@@ -37,21 +49,21 @@ public class PiocheObjectifParcelle extends ArrayList<ObjectifParcelle> implemen
      * Permet d'initialiser la pioche en lui ajoutant les objectifs de parcelles
      */
     private void creePiocheObjectifsParcelle() {
-        add(new ObjectifParcelle(2, motifParDefaut2Parcelles));
-        add(new ObjectifParcelle(3, motifParDefaut2Parcelles));
-        add(new ObjectifParcelle(5, motifParDefaut3Parcelles));
-        add(new ObjectifParcelle(4, motifParDefaut3Parcelles));
-        add(new ObjectifParcelle(3, motifParDefaut2Parcelles));
-        add(new ObjectifParcelle(3, motifParDefaut2Parcelles));
-        add(new ObjectifParcelle(4, motifParDefaut3Parcelles));
-        add(new ObjectifParcelle(3, motifParDefaut2Parcelles));
-        add(new ObjectifParcelle(2, motifParDefaut2Parcelles));
-        add(new ObjectifParcelle(2, motifParDefaut2Parcelles));
-        add(new ObjectifParcelle(4, motifParDefaut3Parcelles));
-        add(new ObjectifParcelle(4, motifParDefaut3Parcelles));
-        add(new ObjectifParcelle(5, motifParDefaut3Parcelles));
-        add(new ObjectifParcelle(4, motifParDefaut3Parcelles));
-        add(new ObjectifParcelle(3, motifParDefaut2Parcelles));
+        add(new ObjectifParcelle(1, motifDiagonale2Parcelles));
+        add(new ObjectifParcelle(1, motifDiagonale2Parcelles));
+        add(new ObjectifParcelle(1, motifDiagonale2Parcelles));
+        add(new ObjectifParcelle(2, motifDiagonale3Parcelles));
+        add(new ObjectifParcelle(2, motifDiagonale3Parcelles));
+        add(new ObjectifParcelle(2, motifDiagonale3Parcelles));
+        add(new ObjectifParcelle(3, motifDecale));
+        add(new ObjectifParcelle(3, motifDecale));
+        add(new ObjectifParcelle(3, motifDecale));
+        add(new ObjectifParcelle(4, motifLosange));
+        add(new ObjectifParcelle(4, motifLosange));
+        add(new ObjectifParcelle(4, motifLosange));
+        add(new ObjectifParcelle(4, motifLosange));
+        add(new ObjectifParcelle(4, motifLosange));
+        add(new ObjectifParcelle(4, motifLosange));
     }
 
 
