234d3c9e729ffb432b75797c5bfc891e55a19bf0
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-02-05 13:45:17 +0100

    fix: Modification d'une méthode dans le motif Losange
    Changement d'une conditiion dans le motif Losange #209

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/motif/MotifLosange.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/motif/MotifLosange.java
@@ -8,18 +8,18 @@ public class MotifLosange extends Motif{
      * Constructeur par defaut
      * @param parcelleCouleur00 Parcelle de couleur de coordonnees 0, 0 (par exemple, peut être 2, 0)
      * @param parcelleCouleur11 Parcelle de couleur de coordonnees 1, 1 (par exemple, peut être 3, 1)
-     * @param parcelleCouleur1m1 Parcelle de couleur de coordonnees -1, 1 (par exemple, peut être 1, 1)
-     * @param parcelleCouleur20 Parcelle de couleur de coordonnees 0, 2 (par exemple, peut être 2, 2)
+     * @param parcelleCouleurm11 Parcelle de couleur de coordonnees -1, 1 (par exemple, peut être 1, 1)
+     * @param parcelleCouleur02 Parcelle de couleur de coordonnees 0, 2 (par exemple, peut être 2, 2)
      */
-    public MotifLosange(ParcelleCouleur parcelleCouleur00, ParcelleCouleur parcelleCouleur11, ParcelleCouleur parcelleCouleur1m1, ParcelleCouleur parcelleCouleur20) {
-        if (parcelleCouleur00 == null || parcelleCouleur11 == null || parcelleCouleur1m1 == null || parcelleCouleur20 == null) {
+    public MotifLosange(ParcelleCouleur parcelleCouleur00, ParcelleCouleur parcelleCouleur11, ParcelleCouleur parcelleCouleurm11, ParcelleCouleur parcelleCouleur02) {
+        if (parcelleCouleur00 == null || parcelleCouleur11 == null || parcelleCouleurm11 == null || parcelleCouleur02 == null) {
             throw new IllegalArgumentException("Les arguments ne doivent pas être nuls");
         }
         tabParcelles = new ParcelleCouleur[4];
         tabParcelles[0] = parcelleCouleur00;
         tabParcelles[1] = parcelleCouleur11;
-        tabParcelles[2] = parcelleCouleur1m1;
-        tabParcelles[3] = parcelleCouleur20;
+        tabParcelles[2] = parcelleCouleurm11;
+        tabParcelles[3] = parcelleCouleur02;
     }
 
     @Override
@@ -29,22 +29,14 @@ public class MotifLosange extends Motif{
         for (int i = 1; i < 6; i++) {
             allOrientation[i][0] = allOrientation[0][0];
             allOrientation[i][1] = getVoisin(i, allOrientation[i][0].position(), tabParcelles[1].couleur());
-            allOrientation[i][2] = getVoisin(4, allOrientation[i][1].position(), tabParcelles[2].couleur());
-            allOrientation[i][3] = getVoisin(5, allOrientation[i][1].position(), tabParcelles[3].couleur());
+            allOrientation[i][2] = allOrientation[i - 1][1];
+            allOrientation[i][3] = getVoisin(i - 1, allOrientation[i][1].position(), tabParcelles[3].couleur());
         }
         return allOrientation;
     }
 
     @Override
     public ParcelleCouleur[][] getOrientationForIA() {
-        ParcelleCouleur[][] allOrientation = new ParcelleCouleur[12][4];
-        System.arraycopy(getOrientation(), 0, allOrientation, 0, 6);
-        for (int i = 6; i < 12; i++) {
-            allOrientation[i][0] = allOrientation[0][0];
-            allOrientation[i][1] = allOrientation[i-6][2];
-            allOrientation[i][2] = allOrientation[i-6][1];
-            allOrientation[i][3] = allOrientation[i-6][3];
-        }
-        return allOrientation;
+        return getOrientation();
     }
 }
