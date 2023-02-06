4be54eeb72d2a10ee8fecce5b59d94c65a15bba2
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-02-06 01:49:06 +0100

    task: Reeordonne les priorités pour la StratégieParcelle
    Prend en premier l'objectif pour compléter ces objectifs, puis pose des parcelles, puis le jardinier (car objectif valant le + de points), panda puis irrigation #224

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/StrategieParcelle.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/StrategieParcelle.java
@@ -13,14 +13,19 @@ public class StrategieParcelle implements Strategie {
 
     @Override
     public Plaquette.ActionPossible choisiActionTour(boolean[] actionsRealiseesTour, List<Objectif> objectifs) {
-        Plaquette.ActionPossible parcelle = Plaquette.ActionPossible.PARCELLE;
-        if (!actionsRealiseesTour[parcelle.ordinal()]) {
-            return parcelle;
-        }
-
         Plaquette.ActionPossible objectif = Plaquette.ActionPossible.OBJECTIF;
         if (!actionsRealiseesTour[objectif.ordinal()] && (objectifs.size() < Joueur.NOMBRE_OBJECTIFS_MAX)) {
             return objectif;
+        }
+
+        Plaquette.ActionPossible parcelle = Plaquette.ActionPossible.PARCELLE;
+        if (!actionsRealiseesTour[objectif.ordinal()]) {
+            return parcelle;
+        }
+
+        Plaquette.ActionPossible jardinier = Plaquette.ActionPossible.JARDINIER;
+        if (!actionsRealiseesTour[jardinier.ordinal()]) {
+            return jardinier;
         }
 
         Plaquette.ActionPossible panda = Plaquette.ActionPossible.PANDA;
@@ -28,6 +33,6 @@ public class StrategieParcelle implements Strategie {
             return panda;
         }
 
-        return Plaquette.ActionPossible.JARDINIER;
+        return Plaquette.ActionPossible.IRRIGATION;
     }
 }
