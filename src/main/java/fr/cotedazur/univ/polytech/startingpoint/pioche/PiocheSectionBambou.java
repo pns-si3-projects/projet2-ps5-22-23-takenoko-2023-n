cbd1ef3e03bbeef3e99f337b1c5910ff986d9c8c
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-06 16:28:07 +0100

    task: PiocheSectionBambou infinie -> suppression méthodes inutiles
    #239

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheSectionBambou.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/pioche/PiocheSectionBambou.java
@@ -4,92 +4,19 @@ import fr.cotedazur.univ.polytech.startingpoint.jeu.Couleur;
 import fr.cotedazur.univ.polytech.startingpoint.pieces.SectionBambou;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Random;
-
 /**
  * Représente la pioche de sections de bambou.
  * @author équipe N
  */
 public class PiocheSectionBambou {
-    // Définition des attributs
-
-    private final Random random;
-    private int nombreSectionsBambouVert;
-    private int nombreSectionsBambouRose;
-    private int nombreSectionsBambouJaune;
-
-
-    // Définition des constructeurs
-
-    /**
-     * Construit une pioche de 90 sections de bambous
-     * @param random un objet Random pour l'aléatoire de la pioche
-     */
-    public PiocheSectionBambou(@NotNull Random random) {
-        this.random = random;
-        nombreSectionsBambouVert = 36;
-        nombreSectionsBambouRose = 24;
-        nombreSectionsBambouJaune = 30;
-    }
-
-
-    // Accesseurs
-
-    /**
-     * Renvoie le nombre de sections de bambou de la couleur demandée
-     * @param couleur la couleur demandée
-     * @return le nombre de sections de bambous de la couleur demandée
-     */
-    public int getNombreSectionsBambou(@NotNull Couleur couleur) {
-        return switch (couleur) {
-            case VERTE -> nombreSectionsBambouVert;
-            case ROSE -> nombreSectionsBambouRose;
-            case JAUNE -> nombreSectionsBambouJaune;
-        };
-    }
-
-    /**
-     * Renvoie si la pioche n'a plus de sections de bambou de la couleur demandée
-     * @param couleur la couleur demandée
-     * @return {@code true} si la pioche n'a plus de sections de bambou de la couleur demandée
-     */
-    public boolean isEmpty(@NotNull Couleur couleur) {
-        return getNombreSectionsBambou(couleur) == 0;
-    }
-
-
     // Méthodes d'utilisation
 
     /**
      * Renvoie une section de bambou de la couleur désignée
      * @param couleur la couleur de la section de bambou
      * @return la section de bambou piochée
-     * @implNote la pioche de la couleur demandée ne doit pas être vide
      */
     public SectionBambou pioche(@NotNull Couleur couleur) {
-        boolean isNotEmpty = !isEmpty(couleur);
-        assert isNotEmpty : "La pioche de sections de bambou est vide";
-
-        int taille = getNombreSectionsBambou(couleur);
-        int positionBambou = random.nextInt(taille);
-        if (positionBambou < 0 || positionBambou >= taille) {
-            throw new ArithmeticException();
-        }
-
-        return retireSectionBambou(couleur);
-    }
-
-    /**
-     * Renvoie une section de bambou de la couleur désignée
-     * @param couleur la couleur de la section de bambou
-     * @return la section de bambou piochée
-     */
-    private SectionBambou retireSectionBambou(Couleur couleur) {
-        switch (couleur) {
-            case VERTE -> --nombreSectionsBambouVert;
-            case ROSE -> --nombreSectionsBambouRose;
-            case JAUNE -> --nombreSectionsBambouJaune;
-        }
         return new SectionBambou(couleur);
     }
 
@@ -98,7 +25,6 @@ public class PiocheSectionBambou {
 
     @Override
     public String toString() {
-        return "Pioche de bambous : " + nombreSectionsBambouVert + " verts, "
-                + nombreSectionsBambouRose + " roses et " + nombreSectionsBambouJaune + " jaunes";
+        return "Pioche de sections de bambou";
     }
 }
