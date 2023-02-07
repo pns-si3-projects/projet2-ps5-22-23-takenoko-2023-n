90a2bc24cabc14bad7282dd88e2dd1ac759f49c8
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-07 16:27:58 +0100

    task: Ajout fichier lecture fichier csv
    #255

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ReadCSV.java
@@ -0,0 +1,45 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class ReadCSV {
+    private static final Path cheminFichier = FileSystems.getDefault().getPath("stats", "data.csv");
+
+    public static void main(String[] args) {
+        CSVParser csvParser = new CSVParserBuilder()
+                .withSeparator(',')
+                .withIgnoreQuotations(true)
+                .build();
+
+        CSVReader csvReader = null;
+        try {
+            csvReader = new CSVReaderBuilder(new FileReader(cheminFichier.toFile()))
+                    .withCSVParser(csvParser)
+                    .build();
+        } catch (FileNotFoundException e) {
+            throw new AssertionError(e);
+        }
+
+        //Read CSV line by line and use the string array as you want
+        String[] nextLine;
+        try {
+            while ((nextLine = csvReader.readNext()) != null) {
+                System.out.println(Arrays.toString(nextLine));
+            }
+        } catch (CsvValidationException | IOException e) {
+            throw new AssertionError(e);
+        }
+
+    }
+}
