86d55d304b497e3d877be480ddafecaf18a348fc
amandinemart1 <amandine.martin@etu.unic-cotedazur.fr>
2023-02-08 17:33:25 +0100

    task : modification actionTour de StrategieComplete
    #279

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/StrategieComplete.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/StrategieComplete.java
@@ -2,6 +2,7 @@ package fr.cotedazur.univ.polytech.startingpoint.joueur;
 
 
 import fr.cotedazur.univ.polytech.startingpoint.jeu.Couleur;
+import fr.cotedazur.univ.polytech.startingpoint.jeu.GestionTours;
 import fr.cotedazur.univ.polytech.startingpoint.jeu.Position;
 import fr.cotedazur.univ.polytech.startingpoint.motif.GestionnairePossibiliteMotif;
 import fr.cotedazur.univ.polytech.startingpoint.objectif.*;
@@ -43,7 +44,7 @@ public class StrategieComplete implements Strategie {
         }
 
         Plaquette.ActionPossible parcelle = Plaquette.ActionPossible.PARCELLE;
-        if (!actionsRealiseesTour[parcelle.ordinal()] && !getObjectifParcelle(objectifs).isEmpty() ) {
+        if (!actionsRealiseesTour[parcelle.ordinal()] && !getObjectifParcelle(objectifs).isEmpty() && !piochesVides[GestionTours.PiochesPossibles.PARCELLE.ordinal()]) {
             return parcelle;
         }
 
@@ -134,11 +135,13 @@ public class StrategieComplete implements Strategie {
     public void actionPanda(Plateau plateau, List<Objectif> objectifs, Plaquette plaquette) {
         List<ObjectifPanda> objectifPandas = getObjectifPanda(objectifs);
         for (ObjectifPanda objectifPanda : objectifPandas) {
+            // si objectifPanda est de trois sections bambou de different couleur
             if(objectifPanda.getBambousAManger().size() == 3) {
                 Couleur couleurVoulue = plaquetteCouleurManquante(plaquette);
                 Position positionDeplacer = parcelleCouleurVoulue(plateau, couleurVoulue);
                 plateau.deplacementPanda(positionDeplacer);
             }
+            // objectifPanda avec deux sectionBambou de meme couleur
             else {
                 Couleur couleur = objectifPanda.getBambousAManger().get(0).getCouleur();
                 Position positionDeplacer = parcelleCouleurVoulue(plateau, couleur);
@@ -146,12 +149,18 @@ public class StrategieComplete implements Strategie {
             }
             break;
         }
-
     }
 
-
+    /**
+     * retourne une position disponible pour le deplacement a la couleur souhaitee
+     * @param plateau le plateau
+     * @param couleur la couleur voulue
+     * @return la position qui et disponible pour le deplecement et de la couleur voulue.
+     * Si aucune parcelle de la couluer voulue n'est disponible on renvoie le premier position
+     */
     public Position parcelleCouleurVoulue(Plateau plateau, Couleur couleur) {
         Position positionVoulue = null;
+        //recuperation des deplacement possible
         List<Position> possitionPossible = GestionPersonnages.deplacementsPossibles(plateau.getParcelleEtVoisinesList(),plateau.getPanda().getPosition());
         for(Position position : possitionPossible) {
             Optional<Parcelle> parcelleOptional = GestionParcelles.chercheParcelle(plateau.getParcelles(), position);
@@ -163,9 +172,16 @@ public class StrategieComplete implements Strategie {
                 }
             }
         }
+        // si aucune parcelle de la couleur voulue n'est disponible
         positionVoulue = possitionPossible.get(0);
         return positionVoulue;
     }
+
+    /**
+     * renvoie la couleur manquante pour objectifPanda de 3 bambou
+     * @param plaquette la plaquette
+     * @return la couleur manquante
+     */
     public Couleur plaquetteCouleurManquante(Plaquette plaquette) {
         if (GestionnaireObjectifs.countCouleurSectionBambou(plaquette.getReserveBambousManges(), Couleur.VERTE) == 0) {return Couleur.VERTE; }
         if (GestionnaireObjectifs.countCouleurSectionBambou(plaquette.getReserveBambousManges(), Couleur.ROSE) == 0) {return Couleur.ROSE; }
