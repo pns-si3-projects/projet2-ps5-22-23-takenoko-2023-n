60869a41e243eda96a973c02400241771061fa3b
Fabien-Airaud <fabien.airaud@etu.unice.fr>
2023-02-09 17:59:41 +0100

    test: Correction tests modes de jeu
    #300

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/ArgumentPossibleMainTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/ArgumentPossibleMainTest.java
@@ -9,7 +9,6 @@ class ArgumentPossibleMainTest {
     ArgumentPossibleMain thousands;
     ArgumentPossibleMain demo;
     ArgumentPossibleMain csv;
-    ArgumentPossibleMain presentation;
 
 
     @BeforeEach
@@ -17,7 +16,6 @@ class ArgumentPossibleMainTest {
         thousands = ArgumentPossibleMain.THOUSANDS;
         demo = ArgumentPossibleMain.DEMO;
         csv = ArgumentPossibleMain.CSV;
-        presentation = ArgumentPossibleMain.PRESENTATION;
     }
 
 
@@ -26,7 +24,6 @@ class ArgumentPossibleMainTest {
         assertTrue(thousands.isThousands());
         assertFalse(demo.isThousands());
         assertFalse(csv.isThousands());
-        assertFalse(presentation.isThousands());
     }
 
     @Test
@@ -34,7 +31,6 @@ class ArgumentPossibleMainTest {
         assertFalse(thousands.isDemo());
         assertTrue(demo.isDemo());
         assertFalse(csv.isDemo());
-        assertFalse(presentation.isDemo());
     }
 
     @Test
@@ -42,15 +38,6 @@ class ArgumentPossibleMainTest {
         assertFalse(thousands.isCsv());
         assertFalse(demo.isCsv());
         assertTrue(csv.isCsv());
-        assertFalse(presentation.isCsv());
-    }
-
-    @Test
-    void isPresentation() {
-        assertFalse(thousands.isPresentation());
-        assertFalse(demo.isPresentation());
-        assertFalse(csv.isPresentation());
-        assertTrue(presentation.isPresentation());
     }
 
 
@@ -59,6 +46,5 @@ class ArgumentPossibleMainTest {
         assertEquals("--2thousands", thousands.toString());
         assertEquals("--demo", demo.toString());
         assertEquals("--csv", csv.toString());
-        assertEquals("--presentation", presentation.toString());
     }
 }
