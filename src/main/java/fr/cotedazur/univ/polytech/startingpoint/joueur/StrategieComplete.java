a8f760616907015317bbe0cb35ac4a9bf82ab362
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-02-09 10:59:26 +0100

    fix: mise à jour parties
    #277

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/StrategieComplete.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/joueur/StrategieComplete.java
@@ -28,13 +28,14 @@ public class StrategieComplete implements Strategie {
     public Plaquette.ActionPossible choisiActionTour(boolean[] actionsRealiseesTour, List<Objectif> objectifs, Plateau plateau, boolean[] piochesVides) {
 
         Plaquette.ActionPossible objectif = Plaquette.ActionPossible.OBJECTIF;
-        if (!actionsRealiseesTour[objectif.ordinal()] && (objectifs.size()<Joueur.NOMBRE_OBJECTIFS_MAX) && (!piochesVides[GestionTours.PiochesPossibles.OBJ_PANDA.ordinal()]  |
+        if (!actionsRealiseesTour[objectif.ordinal()] && (objectifs.size()<Joueur.NOMBRE_OBJECTIFS_MAX) && (!piochesVides[GestionTours.PiochesPossibles.OBJ_PANDA.ordinal()]  ||
                 !piochesVides[GestionTours.PiochesPossibles.OBJ_PARCELLE.ordinal()] | !piochesVides[GestionTours.PiochesPossibles.OBJ_JARDINIER.ordinal()] )) {
             return objectif;
         }
 
         Plaquette.ActionPossible irrigation = Plaquette.ActionPossible.IRRIGATION;
-        if ( (plateau.getBambous().length == 0 /* ou parcelle adversaire autre methode en plus */ && !actionsRealiseesTour[irrigation.ordinal()]) || premierTour ) {
+        if (!piochesVides[GestionTours.PiochesPossibles.IRRIGATION.ordinal()] &&
+                (plateau.getBambous().length == 0 /* ou parcelle adversaire autre methode en plus */ && !actionsRealiseesTour[irrigation.ordinal()]) || premierTour ) {
             if(premierTour) premierTour = false;
             return irrigation;
         }
