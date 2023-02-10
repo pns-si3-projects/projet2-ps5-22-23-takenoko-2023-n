da0ad6278b81ecb243e739dfbb5e159302ffda73
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-02-10 11:25:51 +0100

    fix: Enlever la classe exception non utilisées
    #277

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/objectif/MotifNonValideException.java
+++
@@ -1,9 +0,0 @@
-package fr.cotedazur.univ.polytech.startingpoint.objectif;
-
-import fr.cotedazur.univ.polytech.startingpoint.parcelle.ParcelleCouleur;
-
-public class MotifNonValideException extends Exception{
-    public MotifNonValideException(){
-        super("Il y a des parcelles qui ne sont pas voisines");
-    }
-}
